6e49a2019ec259a4194748a897f062dd2fc79f57
mingyu <mingyu1218@naver.com>
2022-10-25 17:52:51 +0900

    feat: App.tsx 라우터 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import "./App.css";
 import BackgroundVideo from "./components/atoms/common/BackgroundVideo";
+import LandingPage from "./pages/landing/LandingPage";
 import Background from "./styles/Background";
 
 function App() {
   return (
     <div className="App">
-      <Background></Background>
+      <Background>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Background>
     </div>
   );
 }
