05dc2596b3dca6e11158e36292d11c43943b4803
swoody <swoody1101@gmail.com>
2022-10-27 13:42:41 +0900

    feat: Font Entity 생성

---
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/Font.java
@@ -0,0 +1,31 @@
+package com.da_ta.backend.letter.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Font {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fontId;
+
+    @NotNull
+    private String fontName;
+
+    @Builder
+    public Font(Long fontId, String fontName) {
+        this.fontId = fontId;
+        this.fontName = fontName;
+    }
+}
