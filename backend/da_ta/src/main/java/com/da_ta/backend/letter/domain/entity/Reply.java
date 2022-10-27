dbda4de8301d6691a4fc34586fe622e2471e7a4e
swoody <swoody1101@gmail.com>
2022-10-27 23:43:24 +0900

    feat: Reply Entity 추가

---
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/Reply.java
@@ -0,0 +1,28 @@
+package com.da_ta.backend.letter.domain.entity;
+
+import com.da_ta.backend.common.domain.CommonEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "reply_id"))
+@Entity
+public class Reply extends CommonEntity {
+
+    @NotNull
+    private Long originLetterId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "letter_id")
+    @Column(name = "reply_letter_id")
+    private Letter letter;
+
+    private boolean isRead;
+}
