47c6cb1c33be5a704e5b3e87f05ca9f8404e00f6
mingyu <mingyu1218@naver.com>
2022-10-27 14:06:49 +0900

    feat: Title 컴포넌트

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -1,31 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 import BackgroundVideo from "../components/atoms/BackgroundVideo";
+import Title from "../components/atoms/Title";
 import Button from "./../components/atoms/Button";
 
 const TestPage = () => {
   return (
-    <TestContainer>
+    <>
       <BackgroundVideo
         isBlur={true}
         path={`${process.env.PUBLIC_URL}/assets/video/bg1.mp4`}
       />
-      <Button
-        text="보 내 기"
-        fontSize="1.5rem"
-        height="5rem"
-        width="10rem"
-        padding="0.5rem"
-        onClick={() => alert("hello!")}
-        shadow={true}
-        hoverBgOpacity="0.5"
-      />
-    </TestContainer>
+      <TestContainer>
+        <Title text="제목임다"></Title>
+        <Button
+          text="보 내 기"
+          fontSize="1.5rem"
+          height="5rem"
+          width="10rem"
+          padding="0.5rem"
+          onClick={() => alert("hello!")}
+          shadow={true}
+          hoverBgOpacity="0.5"
+        />
+      </TestContainer>
+    </>
   );
 };
 
 const TestContainer = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100vw;
   height: 100vh;
   align-items: center;
