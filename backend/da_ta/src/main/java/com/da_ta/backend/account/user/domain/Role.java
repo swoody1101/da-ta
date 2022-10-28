7e0908957415afedb975b30884298f6890c66a81
giyoon27 <giyoon27@naver.com>
2022-10-28 16:40:14 +0900

    feat: GUEST 인스턴스 삭제

--- backend/da_ta/src/main/java/com/da_ta/backend/account/user/domain/Role.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/account/user/domain/Role.java
@@ -10,16 +10,8 @@ import java.util.Arrays;
 public enum Role {
 
     MEMBER("ROLE_USER", "일반 사용자"),
-    ADMIN("ROLE_ADMIN", "관리자"),
-    GUEST("GUEST", "게스트");
+    ADMIN("ROLE_ADMIN", "관리자");
 
     private final String code;
     private final String displayName;
-
-    public static Role of(String code) {
-        return Arrays.stream(Role.values())
-                .filter(r -> r.getCode().equals(code))
-                .findAny()
-                .orElse(GUEST);
-    }
 }
