9dd4c188fe85dbea92b1c53b52c6946823527e86
giyoon27 <giyoon27@naver.com>
2022-10-28 14:48:46 +0900

    feat: UserController 추가

---
+++ backend/da_ta/src/main/java/com/da_ta/backend/account/user/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.da_ta.backend.account.user.controller;
+
+import com.da_ta.backend.account.user.controller.dto.LoginRequest;
+import com.da_ta.backend.account.user.controller.dto.LoginResponse;
+import com.da_ta.backend.account.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok()
+                .body(userService.login(loginRequest));
+    }
+}
