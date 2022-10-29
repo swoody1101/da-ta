8920260876bc6375a9ccf2749f0552bcd5d33d7e
swoody <swoody1101@gmail.com>
2022-10-29 22:59:11 +0900

    feat: LetterAccusation Entity와 Letter Entity 매핑

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/LetterAccusation.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/LetterAccusation.java
@@ -1,6 +1,23 @@
 package com.da_ta.backend.letter.domain.entity;
 
 import com.da_ta.backend.common.domain.CommonEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.PropertySource;
 
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "letter_accusation_id"))
+@Entity
 public class LetterAccusation extends CommonEntity {
+
+    private Long reporter_id;
+    private String reason;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "letter_id")
+    private Letter letter;
 }
