365ba14834e0cf5d19a0d21c443e463d9c49cc80
swoody <swoody1101@gmail.com>
2022-10-30 01:12:33 +0900

    feat: TextLetterCreateRequest Dto 생성

---
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/controller/dto/TextLetterCreateRequest.java
@@ -0,0 +1,13 @@
+package com.da_ta.backend.letter.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TextLetterCreateRequest {
+}
