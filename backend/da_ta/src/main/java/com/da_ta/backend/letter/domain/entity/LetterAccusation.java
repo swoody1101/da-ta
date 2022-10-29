ecf96079e25fc4183a42d86c8ef029fce613999d
swoody <swoody1101@gmail.com>
2022-10-29 23:02:28 +0900

    feat: @SuperBuilder 어노테이션 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/LetterAccusation.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/LetterAccusation.java
@@ -4,10 +4,12 @@ import com.da_ta.backend.common.domain.CommonEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 
 @Getter
+@SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AttributeOverride(name = "id", column = @Column(name = "letter_accusation_id"))
 @Entity
