f66966a368b2f9e5abce750f4d53b961f4f867cc
swoody <swoody1101@gmail.com>
2022-10-31 16:47:25 +0900

    feat: 예외처리를 위한 CustomException 생성

---
+++ backend/da_ta/src/main/java/com/da_ta/backend/common/domain/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.da_ta.backend.common.domain.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+}
