556e0fac306bdb1ee9ae3ff1d616332e50eedfb7
mingyu <mingyu1218@naver.com>
2022-11-01 01:39:06 +0900

    feat: 편지쓰기 틀 구현

--- frontend/src/components/templates/MainNav.jsx
+++ frontend/src/components/templates/MainNav.jsx
@@ -17,187 +17,157 @@ import { useRecoilState } from "recoil";
 import { loginState } from "./../../recoil/Atoms";
 
 const MainNav = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [scrollY, setScrollY] = useState(0); // 높이 스크롤 값
-  const [headerShow, setHeaderShow] = useState(true); // 헤더 show 여부
-  const [headerMobileMode, setHeaderMobileMode] = useState(false); // pc모드인지 모바일모드인지 여부
-  const [slideMenuToggle, setSlideMenuToggle] = useState(false); // 슬라이딩메뉴 토클
-  const [isLogin, setIsLogin] = useRecoilState(loginState);
+	const [scrollY, setScrollY] = useState(0); // 높이 스크롤 값
+	const [headerShow, setHeaderShow] = useState(true); // 헤더 show 여부
+	const [headerMobileMode, setHeaderMobileMode] = useState(false); // pc모드인지 모바일모드인지 여부
+	const [slideMenuToggle, setSlideMenuToggle] = useState(false); // 슬라이딩메뉴 토클
+	const [isLogin, setIsLogin] = useRecoilState(loginState);
 
-  const handleHeaderShow = () => {
-    if (window.scrollY === 0 || window.scrollY - scrollY < 0) {
-      setHeaderShow(true);
-    } else {
-      setHeaderShow(false);
-    }
-    setScrollY(window.scrollY);
-  };
+	const handleHeaderShow = () => {
+		if (window.scrollY === 0 || window.scrollY - scrollY < 0) {
+			setHeaderShow(true);
+		} else {
+			setHeaderShow(false);
+		}
+		setScrollY(window.scrollY);
+	};
 
-  const handleHeaderMode = () => {
-    setHeaderMobileMode(window.innerWidth <= 1280 ? true : false);
-  };
+	const handleHeaderMode = () => {
+		setHeaderMobileMode(window.innerWidth <= 1280 ? true : false);
+	};
 
-  const handleLogin = () => {
-    setIsLogin(true);
-  };
+	const handleLogin = () => {
+		setIsLogin(true);
+	};
 
-  const handleLogout = () => {
-    setIsLogin(false);
-  };
+	const handleLogout = () => {
+		setIsLogin(false);
+	};
 
-  useEffect(() => {
-    handleHeaderMode();
-    handleHeaderShow();
-  }, []);
+	useEffect(() => {
+		handleHeaderMode();
+		handleHeaderShow();
+	}, []);
 
-  useEffect(() => {
-    console.log(isLogin);
-  }, [isLogin]);
+	useEffect(() => {
+		console.log(isLogin);
+	}, [isLogin]);
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleHeaderShow);
-    window.addEventListener("resize", handleHeaderMode);
-    return () => {
-      window.removeEventListener("scroll", handleHeaderShow);
-      window.removeEventListener("resize", handleHeaderMode);
-    };
-  });
+	useEffect(() => {
+		window.addEventListener("scroll", handleHeaderShow);
+		window.addEventListener("resize", handleHeaderMode);
+		return () => {
+			window.removeEventListener("scroll", handleHeaderShow);
+			window.removeEventListener("resize", handleHeaderMode);
+		};
+	});
 
-  return (
-    <Header headerShow={headerShow}>
-      {headerMobileMode ? (
-        <>
-          <MobileLogo
-            src={`${process.env.PUBLIC_URL}/assets/logo/data_logo.png`}
-            height="64px"
-            onClick={() => navigate("/")}
-          />
-        </>
-      ) : (
-        <>
-          <LogoImage onClick={() => navigate("/")}>DA-TA</LogoImage>
-        </>
-      )}
+	return (
+		<Header headerShow={headerShow}>
+			{headerMobileMode ? (
+				<>
+					<MobileLogo src={`${process.env.PUBLIC_URL}/assets/logo/data_logo.png`} height="4rem" onClick={() => navigate("/")} />
+				</>
+			) : (
+				<>
+					<LogoImage onClick={() => navigate("/")}>DA-TA</LogoImage>
+				</>
+			)}
 
-      {headerMobileMode ? (
-        <HamburgerButtonWrapper>
-          <Button
-            hasBorder={false}
-            width={"4rem"}
-            height={"4rem"}
-            onClick={() => setSlideMenuToggle(true)}
-          >
-            <FontAwesomeIcon
-              icon={faBars}
-              size="2x"
-              style={{ filter: "drop-shadow(0.25rem 0.25rem 0.25rem black)" }}
-            />
-          </Button>
-          {slideMenuToggle && (
-            <TranslucentBackground onClick={() => setSlideMenuToggle(false)} />
-          )}
-          <SlideMenu
-            show={slideMenuToggle}
-            setSlideMenuToggle={setSlideMenuToggle}
-            handleLogin={handleLogin}
-            handleLogout={handleLogout}
-            isLogin={isLogin}
-          />
-        </HamburgerButtonWrapper>
-      ) : (
-        <>
-          <HeaderContents>
-            <HeaderContent onClick={() => navigate("/")}>
-              편지 쓰기
-            </HeaderContent>
-            <HeaderContent onClick={() => navigate("/")}>
-              편지 읽기
-            </HeaderContent>
-            <HeaderContent onClick={() => navigate("/")}>
-              마이페이지
-            </HeaderContent>
-          </HeaderContents>
-          <AuthWrapper>
-            {isLogin ? (
-              <>
-                <Button
-                  hoverBgOpacity="0.3"
-                  fontSize="1.2rem"
-                  height="3rem"
-                  width="11rem"
-                  margin="0 -3.5rem 0 0 "
-                  onClick={handleLogout}
-                >
-                  로그아웃
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button onClick={handleLogin}>
-                  <img
-                    src={`${process.env.PUBLIC_URL}/assets/images/auth/kakao_login.png`}
-                  />
-                </Button>
-              </>
-            )}
-          </AuthWrapper>
-        </>
-      )}
-    </Header>
-  );
+			{headerMobileMode ? (
+				<HamburgerButtonWrapper>
+					<Button hasBorder={false} width={"4rem"} height={"4rem"} onClick={() => setSlideMenuToggle(true)}>
+						<FontAwesomeIcon icon={faBars} size="2x" style={{ filter: "drop-shadow(0.25rem 0.25rem 0.25rem black)" }} />
+					</Button>
+					{slideMenuToggle && <TranslucentBackground onClick={() => setSlideMenuToggle(false)} />}
+					<SlideMenu
+						show={slideMenuToggle}
+						setSlideMenuToggle={setSlideMenuToggle}
+						handleLogin={handleLogin}
+						handleLogout={handleLogout}
+						isLogin={isLogin}
+					/>
+				</HamburgerButtonWrapper>
+			) : (
+				<>
+					<HeaderContents>
+						<HeaderContent onClick={() => navigate("/write")}>편지 쓰기</HeaderContent>
+						<HeaderContent onClick={() => navigate("/read")}>편지 읽기</HeaderContent>
+						<HeaderContent onClick={() => navigate("/mypage")}>마이페이지</HeaderContent>
+					</HeaderContents>
+					<AuthWrapper>
+						{isLogin ? (
+							<>
+								<Button hoverBgOpacity="0.3" fontSize="1.2rem" height="3rem" width="11rem" margin="0 -3.5rem 0 0 " onClick={handleLogout}>
+									로그아웃
+								</Button>
+							</>
+						) : (
+							<>
+								<Button onClick={handleLogin}>
+									<img src={`${process.env.PUBLIC_URL}/assets/images/auth/kakao_login.png`} />
+								</Button>
+							</>
+						)}
+					</AuthWrapper>
+				</>
+			)}
+		</Header>
+	);
 };
 
 const MobileLogo = styled.img`
-  display: flex;
-  flex-direction: row;
-  position: absolute;
-  left: 1rem;
-  padding: 1rem;
-  cursor: pointer;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
+	display: flex;
+	flex-direction: row;
+	position: absolute;
+	left: 1rem;
+	padding: 1rem;
+	cursor: pointer;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
 `;
 
 const HeaderContents = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
 
 const HeaderContent = styled.div`
-  display: flex;
-  margin: 0 2rem 0 2rem;
-  padding: 1rem;
-  cursor: pointer;
-  text-shadow: 1px 1px black;
-  transition: 0.3s ease;
+	display: flex;
+	margin: 0 2rem 0 2rem;
+	padding: 1rem;
+	cursor: pointer;
+	text-shadow: 1px 1px black;
+	transition: 0.3s ease;
 
-  &:hover {
-    color: #ffc08c;
-  }
+	&:hover {
+		color: #ffc08c;
+	}
 
-  ${media.wide`
+	${media.wide`
 		margin: 0 0.5rem 0 0.5rem;
 	`}
 `;
 
 const AuthWrapper = styled.div`
-  display: flex;
-  position: absolute;
-  right: 12rem;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	position: absolute;
+	right: 12rem;
+	align-items: center;
+	justify-content: center;
 `;
 
 const HamburgerButtonWrapper = styled.div`
-  display: flex;
-  position: absolute;
-  right: 5rem;
-  align-items: center;
-  justify-content: center;
+	display: flex;
+	position: absolute;
+	right: 5rem;
+	align-items: center;
+	justify-content: center;
 `;
 
 export default MainNav;
--- frontend/src/pages/letter_write/LetterWritePage.jsx
+++ frontend/src/pages/letter_write/LetterWritePage.jsx
@@ -1,19 +1,84 @@
 import React, { useState } from "react";
-import { CenterWrapper, Wrapper } from "./../../styles/Wrapper";
+import { CenterWrapper, RowCenterWrapper, Wrapper } from "./../../styles/Wrapper";
 import LetterToggleButton from "./../../components/atoms/letter_write/LetterToggleButton";
+import styled from "styled-components";
+import Button from "./../../components/atoms/Button";
+import BackgroundVideo from "./../../components/atoms/BackgroundVideo";
+import BackgroundGradient from "./../../components/atoms/BackgroundGradient";
+import { LetterTextArea } from "../../components/atoms/TextArea";
+import { ColorTypes } from "./../../constants/Colors";
+import { media } from "../../utils/styleUtil";
 
 const LetterWritePage = () => {
-  const [act, setAct] = useState(true);
+	const MOBILE_WIDTH = "90vw";
+	const MOBILE_LETTER_HEIGHT = "126vw";
 
-  return (
-    <>
-      <CenterWrapper>
-        Letter Write Page
-        <LetterToggleButton isActive={act}>편지지</LetterToggleButton>
-        <LetterToggleButton isActive={!act}>도화지</LetterToggleButton>
-      </CenterWrapper>
-    </>
-  );
+	const [act, setAct] = useState(true); // [편지지,도화지] 토글
+	const [letterDesign, setLetterDesign] = useState("default"); // 편지지 디자인 이름
+
+	return (
+		<>
+			<BackgroundGradient start={"E2AAFD"} end={"FFDFC2"} />
+			<RowCenterWrapper>
+				<ContentBlock margin="2rem 0 0 0" mWidth={MOBILE_WIDTH}>
+					<LetterToggleButton category="write" act={act} setAct={setAct} width="50%">
+						편지지
+					</LetterToggleButton>
+					<LetterToggleButton category="draw" act={act} setAct={setAct} width="50%">
+						도화지
+					</LetterToggleButton>
+				</ContentBlock>
+				<ContentBlock mWidth={MOBILE_WIDTH}>
+					<Spacer act={act} />
+				</ContentBlock>
+				<ContentBlock height="35rem" mWidth={MOBILE_WIDTH} mHeight={MOBILE_LETTER_HEIGHT}>
+					<LetterImg src={`${process.env.PUBLIC_URL}/assets/images/letter/${letterDesign}.png`} />
+					<LetterTextArea />
+				</ContentBlock>
+				<ContentBlock height="5rem" alignItems="center" justifyContent="space-between" mWidth={MOBILE_WIDTH}>
+					<Button hoverBgOpacity="0.2" fontSize="1.2rem" height="3rem" width="8rem" margin="0 0 0 8%" shadow={true}>
+						지우기
+					</Button>
+					<Button hoverBgOpacity="0.2" fontSize="1.2rem" height="3rem" width="8rem" margin="0 8% 0 0" shadow={true}>
+						보내기
+					</Button>
+				</ContentBlock>
+			</RowCenterWrapper>
+		</>
+	);
 };
+
+const ContentBlock = styled.div`
+	display: flex;
+	position: relative;
+	flex-direction: row;
+	width: 25rem;
+	height: ${(props) => props.height};
+	align-items: ${(props) => props.alignItems};
+	justify-content: ${(props) => props.justifyContent};
+	margin: ${(props) => props.margin};
+
+	${media.phone`
+    width: ${(props) => props.mWidth};
+    height: calc(${(props) => props.mHeight});
+  `}
+`;
+
+const LetterImg = styled.img`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+`;
+
+const Spacer = styled.div`
+	display: flex;
+	width: 100%;
+	height: 0.5rem;
+	transition: 0.2s ease;
+	border-bottom: 1px solid #d9d9d9;
+	background-color: ${(props) => (props.act ? ColorTypes.LETTER_WRITE_COLOR : ColorTypes.LETTER_DRAW_COLOR)};
+`;
 
 export default LetterWritePage;
