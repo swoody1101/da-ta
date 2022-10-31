4d4abc37deb34104731b4d9a8be52be9fbb7fae0
swoody <swoody1101@gmail.com>
2022-10-31 16:48:39 +0900

    feat: FontRepository 생성

---
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/FontRepository.java
@@ -0,0 +1,7 @@
+package com.da_ta.backend.letter.domain.repository;
+
+import com.da_ta.backend.letter.domain.entity.Font;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FontRepository extends JpaRepository<Font, Long> {
+}
