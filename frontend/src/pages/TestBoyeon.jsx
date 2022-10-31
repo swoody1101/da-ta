5ccb735b7329ef8c8fa3d4ac96d60a093f0071c8
kby <tetepiti149@gmail.com>
2022-10-31 15:25:06 +0900

    feat: 마이페이지 틀 구성

--- frontend/src/pages/TestBoyeon.jsx
+++ frontend/src/pages/TestBoyeon.jsx
@@ -1,14 +1,37 @@
 import React from "react";
-import BackgroundGradient from "../components/atoms/BackgroundGradient";
-import Wave from "../components/atoms/Wave";
+import styled from "styled-components";
+import { BackgroundGradient } from "../components/atoms/BackgroundGradient";
+import { Wave } from "../components/atoms/Wave";
 
+//Todo : 해수면 높이 올리기
 const TestBoyeon = () => (
   <>
-    <Wave opacity={.5} frequency={16} isRight={true}></Wave>
-    <Wave opacity={.3} frequency={8} isRight={true}></Wave>
-    <Wave opacity={.4} frequency={13} isRight={false}></Wave>
-    <BackgroundGradient start={'E2AAFD'} end={'FFDFC2'}/>
+    <Wraper>
+      <MypageContents>실험용 찍찍</MypageContents>
+    </Wraper>
+    <Wave opacity={0.5} frequency={16} isRight={true}></Wave>
+    <Wave opacity={0.3} frequency={8} isRight={true}></Wave>
+    <Wave opacity={0.4} frequency={13} isRight={false}></Wave>
+    <BackgroundGradient start={"E2AAFD"} end={"FFDFC2"} />
   </>
-)
+);
 
-export default TestBoyeon
+const Wraper = styled.div`
+  display: flex;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MypageContents = styled.div`
+  display: flex;
+  height: 656px;
+  width: 900px;
+  justify-content: center;
+  align-items: center;
+  background-color: #f5f5f5;
+  z-index: 1;
+  border-radius: 20px;
+`;
+
+export default TestBoyeon;
