b205a71c7b32731aad9be887c467a288084a8fd3
kby <tetepiti149@gmail.com>
2022-10-31 14:26:44 +0900

    feat: 헤더 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,20 @@ import LandingPage from "./pages/landing/LandingPage";
 import TestPage from "./pages/TestPage";
 import TestPageYoon from "./pages/TestPageYoon";
 import Background from "./styles/Background";
-import TestBoyeon from './pages/TestBoyeon';
+import TestBoyeon from "./pages/TestBoyeon";
+import MainNav from "./components/templates/MainNav";
 
 function App() {
   return (
     <div className="App">
       <Background>
         <BrowserRouter>
+          <MainNav />
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/test" element={<TestPage />} />
             <Route path="/testyoon" element={<TestPageYoon />} />
+            <Route path="/bytest" element={<TestBoyeon />} />
           </Routes>
         </BrowserRouter>
       </Background>
