fa0ea6099fa1872333c4e07ce9efd7dcaaec63b8
kby <tetepiti149@gmail.com>
2022-10-31 14:19:51 +0900

    feat: 테스트페이지에 파도 효과 적용

--- frontend/src/pages/TestBoyeon.jsx
+++ frontend/src/pages/TestBoyeon.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import BackgroundGradient from "../components/atoms/BackgroundGradient";
+import Wave from "../components/atoms/Wave";
 
 const TestBoyeon = () => (
-  <BackgroundGradient start={'E2AAFD'} end={'FFDFC2'}></BackgroundGradient>
+  <>
+    <Wave opacity={.5} frequency={16} isRight={true}></Wave>
+    <Wave opacity={.3} frequency={8} isRight={true}></Wave>
+    <Wave opacity={.4} frequency={13} isRight={false}></Wave>
+    <BackgroundGradient start={'E2AAFD'} end={'FFDFC2'}/>
+  </>
 )
 
 export default TestBoyeon
