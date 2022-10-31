81fb9cab7ee28ce5f0accff7a14d23ce580c7d6f
swoody <swoody1101@gmail.com>
2022-10-31 16:52:10 +0900

    feat: 예외 관리 헨들러 GlobalExceptionHandler 생성 및 예외 처리 함수 생성

---
+++ backend/da_ta/src/main/java/com/da_ta/backend/common/controller/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.da_ta.backend.common.controller;
+
+import com.da_ta.backend.common.domain.exception.CustomException;
+import com.da_ta.backend.common.domain.exception.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.da_ta.backend.common.domain.exception.ErrorCode.INTERNAL_SERVER_ERROR;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({CustomException.class})
+    private ResponseEntity<ErrorResponse> handleCustomException(CustomException customException) {
+        return ResponseEntity.status(customException.getErrorCode().getHttpStatus())
+                .body(new ErrorResponse(customException.getErrorCode().getHttpStatus(), customException.getErrorCode().getMessage()));
+    }
+
+    @ExceptionHandler({Exception.class})
+    protected ResponseEntity handleServerException() {
+        return new ResponseEntity(new ErrorResponse(INTERNAL_SERVER_ERROR.getHttpStatus(), INTERNAL_SERVER_ERROR.getMessage()),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
