79073d0280bffdf1f7bbcb72636a92ec7fa1a5ec
김채윤 <only363me@gmail.com>
2022-10-31 16:20:38 +0900

    feat(feat: 메인 배경 atom 추가): feat: 메인 배경 atom 추가

--- frontend/src/pages/TestPageYoon.jsx
+++ frontend/src/pages/TestPageYoon.jsx
@@ -1,51 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import LetterOfBottle from "../components/atoms/LetterOfBottle";
 import Title from "../components/atoms/Title";
-import Button from "../components/atoms/Button";
-import { MainText } from "../components/atoms/Text";
-import Input from "../components/atoms/Input";
-import DropDownInput from "../components/atoms/DropDownInput";
 
-const TestPageYoon = () => {
-  const list = ["aa", "bbb", "cccc", "d"];
+import MainBackgroundSky from "../components/atoms/MainBackgroundSky";
+import MainNav from "./../components/templates/MainNav";
 
-  return (
+
+const TestPageYoon = () => (
     <>
-      <TestContainer>
-        <Title color="black">y의testpage</Title>
-        <LetterOfBottle/>
-        <Button
-          fontSize="1.5rem"
-          height="5rem"
-          width="10rem"
-          padding="0.5rem"
-          onClick={() => alert("hello!")}
-          shadow={true}
-          hoverBgOpacity="0.5"
-        >
-          보 내 기
-        </Button>
-        <MainText margin="5rem 0 0 0">
-          안녕하세요!
-          <br />
-          여기는 '닿다'에요
-        </MainText>
-        <Input />
-        <DropDownInput itemList={list}></DropDownInput>
-
-      </TestContainer>
+    <Title>yoon's test page</Title>
+    <MainNav />
+    <MainBackgroundSky></MainBackgroundSky>
     </>
-  );
-};
+);
 
-const TestContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100vw;
-  height: 100vh;
-  align-items: center;
-  justify-content: center;
-`;
+
 
 export default TestPageYoon;
