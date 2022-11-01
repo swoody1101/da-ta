6258e55d2cdba0e22cdb5a3785f763f9a444b2d2
swoody <swoody1101@gmail.com>
2022-11-01 12:37:16 +0900

    feat: 코드 컨벤션 수정

--- backend/da_ta/src/main/java/com/da_ta/backend/common/domain/exception/ErrorCode.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/common/domain/exception/ErrorCode.java
@@ -10,7 +10,7 @@ public enum ErrorCode {
 
     BACKGROUND_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 편지지입니다."),
     FONT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 폰트입니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저id입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL SERVER ERROR");
 
     private final HttpStatus httpStatus;
