9837ca83eae24731ea533cdc941f5e7acfeead20
swoody <swoody1101@gmail.com>
2022-11-01 16:43:44 +0900

    feat: 예외처리 세분화

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/service/LetterService.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/service/LetterService.java
@@ -3,7 +3,7 @@ package com.da_ta.backend.letter.service;
 import com.da_ta.backend.account.user.domain.entity.User;
 import com.da_ta.backend.account.user.domain.repository.UserRepository;
 import com.da_ta.backend.common.domain.Message;
-import com.da_ta.backend.common.domain.exception.CustomException;
+import com.da_ta.backend.common.domain.exception.NotFoundException;
 import com.da_ta.backend.letter.controller.dto.ImageLetterCreateRequest;
 import com.da_ta.backend.letter.controller.dto.TextLetterCreateRequest;
 import com.da_ta.backend.letter.controller.dto.common.ImageLetterInfo;
@@ -16,7 +16,9 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
-import static com.da_ta.backend.common.domain.exception.ErrorCode.*;
+import static com.da_ta.backend.common.domain.ErrorCode.*;
+import static com.da_ta.backend.common.domain.SuccessCode.IMAGE_LETTER_CREATED;
+import static com.da_ta.backend.common.domain.SuccessCode.TEXT_LETTER_CREATED;
 
 @RequiredArgsConstructor
 @Service
@@ -35,8 +37,7 @@ public class LetterService {
         TextLetterInfo textLetterInfo = textLetterCreateRequest.getTextLetterInfo();
         User user = findUser(textLetterCreateRequest.getUserId());
         Background background = findBackground(textLetterInfo.getBackgroundId());
-        Font font = fontRepository.findById(textLetterInfo.getFontId())
-                .orElseThrow(() -> new CustomException(FONT_NOT_FOUND));
+        Font font = findFont(textLetterInfo.getFontId());
         TextLetter textLetter = TextLetter.builder()
                 .user(user)
                 .ageOption(option.getAgeOption())
@@ -48,7 +49,7 @@ public class LetterService {
                 .build();
         textLetterRepository.save(textLetter);
         floatLetter(textLetter);
-        return new Message("편지를 성공적으로 바다에 띄웠습니다.");
+        return new Message(TEXT_LETTER_CREATED.getMessage());
     }
 
     @Transactional
@@ -67,7 +68,7 @@ public class LetterService {
                 .build();
         imageLetterRepository.save(imageLetter);
         floatLetter(imageLetter);
-        return new Message("편지를 성공적으로 바다에 띄웠습니다.");
+        return new Message(IMAGE_LETTER_CREATED.getMessage());
     }
 
     public void floatLetter(Letter letter) {
@@ -78,11 +79,16 @@ public class LetterService {
 
     public User findUser(Long userId) {
         return userRepository.findById(userId)
-                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
     }
 
     public Background findBackground(Long backgroundId) {
         return backgroundRepository.findById(backgroundId)
-                .orElseThrow(() -> new CustomException(BACKGROUND_NOT_FOUND));
+                .orElseThrow(() -> new NotFoundException(BACKGROUND_NOT_FOUND));
+    }
+
+    public Font findFont(Long fontId) {
+        return fontRepository.findById(fontId)
+                .orElseThrow(() -> new NotFoundException(FONT_NOT_FOUND));
     }
 }
