9cfece02bc96fed93ca2104ebcf93023c6b180a2
swoody <swoody1101@gmail.com>
2022-11-01 11:12:10 +0900

    feat: ImageLetterCreateRequest 컬럼 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/controller/dto/ImageLetterCreateRequest.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/controller/dto/ImageLetterCreateRequest.java
@@ -1,13 +1,16 @@
 package com.da_ta.backend.letter.controller.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import com.da_ta.backend.letter.controller.dto.common.ImageLetterInfo;
+import com.da_ta.backend.letter.controller.dto.common.Option;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class ImageLetterCreateRequest {
+
+    private Long userId;
+    private Option option;
+    private ImageLetterInfo imageLetterInfo;
 }
