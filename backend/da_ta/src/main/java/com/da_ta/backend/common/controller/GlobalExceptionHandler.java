1436bf2f1355d15ba4d653578a41e04759d6e047
swoody <swoody1101@gmail.com>
2022-11-01 16:42:09 +0900

    feat: 예외처리 세분화

--- backend/da_ta/src/main/java/com/da_ta/backend/common/controller/GlobalExceptionHandler.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/common/controller/GlobalExceptionHandler.java
@@ -1,28 +1,26 @@
 package com.da_ta.backend.common.controller;
 
-import com.da_ta.backend.common.domain.exception.CustomException;
-import com.da_ta.backend.common.domain.exception.ErrorResponse;
+import com.da_ta.backend.common.domain.Message;
+import com.da_ta.backend.common.domain.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static com.da_ta.backend.common.domain.exception.ErrorCode.INTERNAL_SERVER_ERROR;
+import static com.da_ta.backend.common.domain.ErrorCode.INTERNAL_SERVER_ERROR;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({CustomException.class})
-    private ResponseEntity<ErrorResponse> handleCustomException(CustomException customException) {
-        customException.getErrorCode();
-        return ResponseEntity.status(customException.getErrorCode().getHttpStatus())
-                .body(new ErrorResponse(customException.getErrorCode().getHttpStatus(),
-                        customException.getErrorCode().getMessage()));
+    @ExceptionHandler({NotFoundException.class})
+    private ResponseEntity<Message> handleNotFoundException(NotFoundException notFoundException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new Message(notFoundException.getErrorCode().getMessage()));
     }
 
     @ExceptionHandler({Exception.class})
     private ResponseEntity handleServerException() {
-        return new ResponseEntity(new ErrorResponse(INTERNAL_SERVER_ERROR.getHttpStatus(),
-                INTERNAL_SERVER_ERROR.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(INTERNAL_SERVER_ERROR.getMessage());
     }
 }
