df7bb0f11f0c1eef02405fa2e4850011220799d2
swoody <swoody1101@gmail.com>
2022-11-01 16:40:45 +0900

    refactor: 클래스명 수정

--- backend/da_ta/src/main/java/com/da_ta/backend/common/domain/exception/CommonException.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/common/domain/exception/CommonException.java
@@ -1,11 +1,12 @@
 package com.da_ta.backend.common.domain.exception;
 
+import com.da_ta.backend.common.domain.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class CustomException extends RuntimeException {
+public class CommonException extends RuntimeException {
 
     private final ErrorCode errorCode;
 }
