c88c912f0fae0dd6de95a5e37f9607e74dbf3e72
김보연 <tetepiti149@naver.com>
2022-11-03 01:34:20 +0900

    feat: 편지함 가져오는동안 로딩 이미지 추가

--- frontend/src/components/molecules/mypage/MypageReceive.jsx
+++ frontend/src/components/molecules/mypage/MypageReceive.jsx
@@ -4,44 +4,47 @@ import { MypageLetter } from "../../atoms/mypage/MypageLetter";
 import { useRecoilState } from "recoil";
 import { mypageRouterState } from "../../../recoil/Atoms";
 import { useState } from "react";
+import Loading from "../Loading";
 
 export const MypageReceive = () => {
   const [selectedIndex, setSelectedIndex] = useRecoilState(mypageRouterState);
   const [letters, setLetters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
   useEffect( async () => {
     setSelectedIndex(1)
-    //스피너 추가
-
     //n ms이후에 api 요청으로 편지를 받아왔다고 가정
-    setTimeout(() => {setLetters([
-      {
-        letterId: 11,
-        letterTitle: '첫번째 받은 답장',
-        userId: 2,
-        userNickName: '거꾸로 타는 보일러',
-        time: new Date(), 
-      },
-      {
-        letterId: 21,
-        letterTitle: '두번째 받은 답장',
-        userId: 4,
-        userNickName: '매우 똑똑한 아이폰 미니',
-        time: new Date(),
-      },
-      {
-        letterId: 31,
-        letterTitle: '세번째 받은 답장',
-        userId: 7,
-        userNickName: '약간 멍청한 갤럭시 S6',
-        time: new Date(),
-      }
-    ])}, 1000)
+    setTimeout(() => {
+      setLetters([
+        {
+          letterId: 11,
+          letterTitle: '첫번째 받은 답장',
+          userId: 2,
+          userNickName: '거꾸로 타는 보일러',
+          time: new Date(), 
+        },
+        {
+          letterId: 21,
+          letterTitle: '두번째 받은 답장',
+          userId: 4,
+          userNickName: '매우 똑똑한 아이폰 미니',
+          time: new Date(),
+        },
+        {
+          letterId: 31,
+          letterTitle: '세번째 받은 답장',
+          userId: 7,
+          userNickName: '약간 멍청한 갤럭시 S6',
+          time: new Date(),
+        }
+      ])
+      setIsLoading(false)
+    }, 1000)
   }, [])
 
   return (
     <>
       {
-        letters.map((letter, index) => <MypageLetter letter={letter} key={index}></MypageLetter>)
+        isLoading ? <Loading /> : letters.map((letter, index) => <MypageLetter letter={letter} key={index}></MypageLetter>)
       }
     </>  
   )
