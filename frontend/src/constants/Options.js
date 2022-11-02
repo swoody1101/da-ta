b9ff7d72484af099e3c3b717d8d0cc59b79f79f0
mingyu <mingyu1218@naver.com>
2022-11-03 01:17:18 +0900

    feat: 옵션 체크 시 state 변경 적용

---
+++ frontend/src/constants/Options.js
@@ -0,0 +1,9 @@
+/**
+ * @author mingyu
+ * @description 편지지 옵션에 대한 상수 정의입니다.
+ */
+
+export const LetterOptions = {
+	FONTS: ["굴림체", "바탕체", "휴먼매직체"],
+	AGES: ["전체", "0대", "10대", "20대", "30대", "40대", "50대", "60대"],
+};
--- frontend/src/constants/Variables.js
+++ frontend/src/constants/Variables.js
@@ -2,5 +2,5 @@
  * @author mingty
  * @description 글자수 등 통용되는 상수에 대해 정의합니다.
  */
-export const MIN_CHAR_COUNT = 140; // 편지지 최소 글자 수
+export const MIN_CHAR_COUNT = 100; // 편지지 최소 글자 수
 export const MAX_CHAR_COUNT = 1000; // 편지지 최대 글자 수
