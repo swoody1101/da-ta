2eb41a5e647bc4eaf0488447b2b686d1eecc0821
mingyu <mingyu1218@naver.com>
2022-11-02 11:19:23 +0900

    feat: 편지지 반응형 크기 조절

--- frontend/src/constants/Sizes.js
+++ frontend/src/constants/Sizes.js
@@ -1,10 +1,12 @@
 const deviceWidth = screen.width;
 const deviceHeight = screen.height;
 
+console.log(deviceHeight);
+
 export const SizeTypes = {
-  PC_LETTER_MARGIN: deviceWidth < 1600 ? "1rem 0 0 0" : "2rem 0 0 0",
-  PC_LETTER_WIDTH: deviceWidth < 1600 ? "20rem" : "26rem",
-  PC_LETTER_HEIGHT: deviceWidth < 1600 ? "28rem" : "36.4rem",
+  PC_LETTER_MARGIN: deviceHeight < 1000 ? "1rem 0 0 0" : "2rem 0 0 0",
+  PC_LETTER_WIDTH: deviceHeight < 1000 ? "20rem" : "26rem",
+  PC_LETTER_HEIGHT: deviceHeight < 1000 ? "28rem" : "36.4rem",
   MOBILE_LETTER_WIDTH: "90vw",
   MOBILE_LETTER_HEIGHT: "126VW",
 };
