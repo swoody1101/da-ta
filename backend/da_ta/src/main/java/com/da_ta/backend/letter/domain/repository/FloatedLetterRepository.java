e3d8d289c9c8bb9aa5e5fbb204c126433647ed6d
swoody <swoody1101@gmail.com>
2022-11-02 21:41:55 +0900

    feat: 쿼리문 수정

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/FloatedLetterRepository.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/FloatedLetterRepository.java
@@ -9,18 +9,18 @@ import java.util.Optional;
 
 public interface FloatedLetterRepository extends JpaRepository<FloatedLetter, Long> {
 
-    @Query(value = "select f.floated_letter_id, f.letter_id, f.user_id " +
+    @Query(value = "select f.floated_letter_id, f.letter_id, f.recipient_id " +
             "from floated_letter f " +
             "join letter l " +
             "using(letter_id) " +
             "where f.is_active = true " +
-            "and f.user_id is null " +
+            "and f.recipient_id is null " +
             "and l.writer_id != :recipientId " +
             "and l.age_option = :ageOption " +
             "and f.floated_letter_id " +
             "NOT IN " +
             "(select floated_letter_id " +
-            "from log " +
+            "from floated_letter_log " +
             "where logged_recipient_id = :recipientId)" +
             "order by l.created_date limit 1;",
             nativeQuery = true)
