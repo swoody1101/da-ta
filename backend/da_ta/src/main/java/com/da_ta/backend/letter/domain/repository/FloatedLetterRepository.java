02b2abff7b4f8b23673978e9ed5b12878806b87c
swoody <swoody1101@gmail.com>
2022-11-02 17:42:52 +0900

    feat: 떠다니는 편지 받기 함수 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/FloatedLetterRepository.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/FloatedLetterRepository.java
@@ -1,7 +1,28 @@
 package com.da_ta.backend.letter.domain.repository;
 
+import com.da_ta.backend.common.domain.Age;
 import com.da_ta.backend.letter.domain.entity.FloatedLetter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface FloatedLetterRepository extends JpaRepository<FloatedLetter, Long> {
+
+    @Query(value = "select f.floated_letter_id, f.letter_id, f.user_id " +
+            "from floated_letter f " +
+            "join letter l " +
+            "using(letter_id) " +
+            "where f.is_active = true " +
+            "and f.user_id is null " +
+            "and l.writer_id != :recipientId " +
+            "and l.age_option = :ageOption " +
+            "and f.floated_letter_id " +
+            "NOT IN " +
+            "(select floated_letter_id " +
+            "from log " +
+            "where logged_recipient_id = :recipientId)" +
+            "order by l.created_date limit 1;",
+            nativeQuery = true)
+    Optional<FloatedLetter> findFloatedLetterByAgeOption(Long recipientId, Age ageOption);
 }
