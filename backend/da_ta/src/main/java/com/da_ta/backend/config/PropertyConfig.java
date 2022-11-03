6e2f5ee81967660aea15bbb9508652429d36e0d2
SSAFY <tgb02087@naver.com>
2022-11-03 16:05:50 +0900

    feat: env파일경로 수정

--- backend/da_ta/src/main/java/com/da_ta/backend/config/PropertyConfig.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/config/PropertyConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("file:../env.properties")
+@PropertySource("file:./env.properties")
 public class PropertyConfig {
 }
