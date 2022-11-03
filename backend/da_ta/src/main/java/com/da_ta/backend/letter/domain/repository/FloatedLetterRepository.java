dfd95a50f630c2900d0888f91fd8bb6a89b3a476
swoody <swoody1101@gmail.com>
2022-11-03 10:30:30 +0900

    feat: 떠다니는 편지 조회 쿼리문 수정

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/FloatedLetterRepository.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/FloatedLetterRepository.java
@@ -4,25 +4,27 @@ import com.da_ta.backend.common.domain.Age;
 import com.da_ta.backend.letter.domain.entity.FloatedLetter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface FloatedLetterRepository extends JpaRepository<FloatedLetter, Long> {
 
-    @Query(value = "select f.floated_letter_id, f.letter_id, f.recipient_id " +
+    @Query(value = "select f.* " +
             "from floated_letter f " +
-            "join letter l " +
-            "using(letter_id) " +
+            "left join letter l " +
+            "on f.letter_id = l.letter_id " +
             "where f.is_active = true " +
             "and f.recipient_id is null " +
             "and l.writer_id != :recipientId " +
-            "and l.age_option = :ageOption " +
+            "and l.age_option like concat('%', :ageOption, '%') " +
             "and f.floated_letter_id " +
             "NOT IN " +
-            "(select floated_letter_id " +
-            "from floated_letter_log " +
-            "where logged_recipient_id = :recipientId)" +
-            "order by l.created_date limit 1;",
+                "(select log.floated_letter_id " +
+                "from floated_letter_log log " +
+                "where logged_recipient_id = :recipientId) " +
+            "order by l.created_date limit 1",
             nativeQuery = true)
-    Optional<FloatedLetter> findFloatedLetterByAgeOption(Long recipientId, Age ageOption);
+    Optional<FloatedLetter> findFloatedLetterByAgeOption(@Param("recipientId") Long recipientId,
+                                                         @Param("ageOption") String ageOption);
 }
