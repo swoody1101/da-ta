68f4914d9fc77941802526a05dc0a343aebadf03
kby <tetepiti149@gmail.com>
2022-11-03 15:08:41 +0900

    test: 편지받기 페이지 레이아웃

--- frontend/src/pages/TestBoyeon.jsx
+++ frontend/src/pages/TestBoyeon.jsx
@@ -1,56 +1,130 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
+import BackgroundVideo from "../components/atoms/BackgroundVideo";
+import { Wrapper } from "../styles/Wrapper";
+import { media } from "../utils/styleUtil";
 import styled from "styled-components";
-import BackgroundGradient from "../components/atoms/BackgroundGradient";
-import { Wave } from "../components/atoms/Wave";
-import { MypageMenuBar } from "../components/molecules/mypage/MypageMenubar";
-import { MypageContentsBox } from "../components/organisms/mypage/MypageContentsBox";
 
-//Todo : 해수면 높이 올리기
 const TestBoyeon = () => {
-  // Todo: MenuBar molecule 의 selectedIndex와 같이 전역으로 관리하기
-  let [selectedIndex, setSelectedIndex] = useState(0);
-
+  const [blur, setBlur] = useState(false);
+  useEffect(async () => {
+    setTimeout(() => {
+      setBlur(true);
+    }, 5000);
+  }, []);
   return (
     <>
-      <Wraper>
-        <MypageWebContents>
-          <MypageMenuBar />
-          <MypageContentsBox />
-        </MypageWebContents>
-      </Wraper>
-      <Wave opacity={0.5} frequency={16} isRight={true}></Wave>
-      <Wave opacity={0.3} frequency={8} isRight={true}></Wave>
-      <Wave opacity={0.4} frequency={13} isRight={false}></Wave>
-      <BackgroundGradient start={"E2AAFD"} end={"FFDFC2"} />
+      <BackgroundVideo
+        isBlur={blur}
+        path={`${process.env.PUBLIC_URL}/assets/video/bg2.mp4`}
+      />
+      <Wrapper style={{ alignItems: "center" }}>
+        <BlurBlur>
+          <p>편지왔다</p>
+          <p>읽어라</p>
+          <div
+            style={{ width: "100px", height: "100px", backgroundColor: "red" }}
+          >
+            병 그림 들어갈 자리
+          </div>
+        </BlurBlur>
+        <Template>
+          <Paper></Paper>
+          <Buttons>
+            <BubbleBtn />
+            <BubbleBtn />
+            <BubbleBtn2 />
+          </Buttons>
+        </Template>
+      </Wrapper>
     </>
   );
 };
 
-const Wraper = styled.div`
+const BlurBlur = styled.div`
   display: flex;
-  min-height: 100vh;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-evenly;
   align-items: center;
+  width: 100%;
+  height: 250px;
+  background-color: black;
+  opacity: 0.6;
+  color: white;
+  font-size: 24px;
 `;
 
-const MypageWebContents = styled.div`
+const Template = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   display: flex;
-  height: 656px;
-  width: 900px;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
-  background-color: #f5f5f5;
-  z-index: 1;
-  border-radius: 20px;
+  height: 80vh;
+  background-color: black;
+  opacity: 0.6;
 
-  @media screen and (max-width: 900px) {
-    width: 100vw;
-    height: 100vh;
-    border-radius: 0px;
-    flex-direction: column;
-    justify-content: flex-start;
-  }
+  ${media.tablet2`
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+  `}
 `;
 
+const Paper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 66vh;
+  width: 44vh;
+  background-color: red;
+  opacity: 0.6;
+
+  ${media.tablet2`
+    width: 90%;
+    height: 70%;
+  `}
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  flex-wrap: wrap-reverse;
+  align-items: space-between;
+  height: 14vh;
+  width: 60vh;
+  background-color: blue;
+  opacity: 0.6;
+  ${media.tablet2`
+    width: 90%;
+    height: 15%;
+  `}
+`;
+
+const BubbleBtn = styled.div`
+  height: 12.5vh;
+  width: 12.5vh;
+  background-color: yellow;
+  opacity: 0.6;
+  border-radius: 50%;
+  ${media.phone`
+    width: 40%;
+    height: 40px;
+    border-radius: 8px;
+  `}
+`;
+
+const BubbleBtn2 = styled.div`
+  height: 12.5vh;
+  width: 12.5vh;
+  background-color: yellow;
+  opacity: 0.6;
+  border-radius: 50%;
+  ${media.phone`
+    display: none;
+  `}
+`;
 export default TestBoyeon;
