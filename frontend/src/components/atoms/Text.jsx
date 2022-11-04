d4e0dc0fbd2708d0ad74b3bced56c65b5ccee12a
mingyu <mingyu1218@naver.com>
2022-11-04 17:05:22 +0900

    fix: text conflict

--- frontend/src/components/atoms/Text.jsx
+++ frontend/src/components/atoms/Text.jsx
@@ -3,10 +3,7 @@
  * @description 여기에 텍스트, 라벨과 관련된 스타일 컴포넌트를 작성합니다.
  */
 import styled from "styled-components";
-<<<<<<< HEAD
-=======
 import { media } from "../../utils/styleUtil";
->>>>>>> 16159da28ec98d40185446ceff8a85879bdff73b
 
 export const MainText = styled.p`
   display: flex;
@@ -35,14 +32,11 @@ export const MainSmallText = styled.p`
   margin: ${(props) => props.margin};
   font-weight: ${(props) => props.fontWeight};
 
-<<<<<<< HEAD
-=======
   ${media.phone`
   font-size: ${(props) => props.mFont_size};
 `}
 `;
 
->>>>>>> 16159da28ec98d40185446ceff8a85879bdff73b
 export const FooterText = styled.p`
   display: flex;
   font-size: 0.8rem;
