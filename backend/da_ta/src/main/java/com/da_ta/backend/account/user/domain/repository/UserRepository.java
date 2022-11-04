b24248a7843b321141599953c0c418ce64365b04
giyoon27 <giyoon27@naver.com>
2022-11-04 20:07:04 +0900

    feat: findByKakaoId 메소드 반환형 수정

--- backend/da_ta/src/main/java/com/da_ta/backend/account/user/domain/repository/UserRepository.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/account/user/domain/repository/UserRepository.java
@@ -3,11 +3,9 @@ package com.da_ta.backend.account.user.domain.repository;
 import com.da_ta.backend.account.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByKakaoId(String kakaoId);
+    User findByKakaoId(String kakaoId);
 
     boolean existsByNickname(String randomNickname);
 }
