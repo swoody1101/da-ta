136f6b9753e5c53dece917d16df956454227b9a6
giyoon27 <giyoon27@naver.com>
2022-11-04 11:01:08 +0900

    feat: LoginResponse 필드 수정

--- backend/da_ta/src/main/java/com/da_ta/backend/account/user/controller/dto/LoginResponse.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/account/user/controller/dto/LoginResponse.java
@@ -1,6 +1,16 @@
 package com.da_ta.backend.account.user.controller.dto;
 
+import com.da_ta.backend.account.user.domain.Role;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginResponse {
 
-    private String accessToken;
+    private Long userId;
+    private String nickname;
+    private Role role;
+    private BanStatusInfo banStatus;
 }
