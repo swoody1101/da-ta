9a1e5dd33fbcec98f019ec1ef477f5f6aaaff2c1
swoody <swoody1101@gmail.com>
2022-11-04 09:42:36 +0900

    feat: 코드 컨벤션 수정

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/controller/LetterController.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/controller/LetterController.java
@@ -53,6 +53,7 @@ public class LetterController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT)
                 .body(letterService.checkReplyReception(replyId));
     }
+
     @PostMapping("/collect/{user_id}/{letter_id}")
     public ResponseEntity<Message> collectLetter(@PathVariable("user_id") Long userId, @PathVariable("letter_id") Long letterId) {
         return ResponseEntity.status(HttpStatus.CREATED)
