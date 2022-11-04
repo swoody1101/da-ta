416b38dba06182e469ce38ed150e89bbeea75d2f
giyoon27 <giyoon27@naver.com>
2022-11-04 11:10:42 +0900

    feat: 로그인 기능 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/account/user/controller/UserController.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/account/user/controller/UserController.java
@@ -4,10 +4,16 @@ import com.da_ta.backend.account.user.controller.dto.LoginRequest;
 import com.da_ta.backend.account.user.controller.dto.LoginResponse;
 import com.da_ta.backend.account.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/user")
@@ -15,9 +21,13 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        HttpHeaders headers = new HttpHeaders();
+        LoginResponse loginResponse = userService.login(loginRequest, headers);
+        log.info("access-token : " + headers);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(userService.login(loginRequest));
+                .headers(headers)
+                .body(loginResponse);
     }
 }
