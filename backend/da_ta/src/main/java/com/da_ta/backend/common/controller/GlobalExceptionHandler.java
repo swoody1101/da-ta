3e29a8ece8e397c4a781222311362e66b344283b
giyoon27 <giyoon27@naver.com>
2022-11-04 11:05:47 +0900

    feat: GlobalExceptionHandler 추가

---
+++ backend/da_ta/src/main/java/com/da_ta/backend/common/controller/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.da_ta.backend.common.controller;
+
+import com.da_ta.backend.common.domain.Message;
+import com.da_ta.backend.common.domain.exception.NotFoundException;
+import com.da_ta.backend.common.domain.exception.WrongAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({NotFoundException.class})
+    private ResponseEntity<Message> handleNotFoundException(NotFoundException notFoundException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new Message(notFoundException.getErrorCode().getMessage()));
+    }
+
+    @ExceptionHandler({WrongAccessException.class})
+    private ResponseEntity<Message> handleNotFoundException(WrongAccessException wrongAccessException) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new Message(wrongAccessException.getErrorCode().getMessage()));
+    }
+}
+
