0fc0154eeca0aac0844de9aa86de51c892e3a2c5
swoody <swoody1101@gmail.com>
2022-11-04 13:50:11 +0900

    feat: BadRequest exception 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/common/controller/GlobalExceptionHandler.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/common/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.da_ta.backend.common.controller;
 
 import com.da_ta.backend.common.domain.Message;
+import com.da_ta.backend.common.domain.exception.BadRequestException;
 import com.da_ta.backend.common.domain.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,5 +15,11 @@ public class GlobalExceptionHandler {
     private ResponseEntity<Message> handleNotFoundException(NotFoundException notFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new Message(notFoundException.getErrorCode().getMessage()));
+    }
+
+    @ExceptionHandler({BadRequestException.class})
+    private ResponseEntity<Message> handleBadRequestException(BadRequestException badRequestException) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new Message(badRequestException.getErrorCode().getMessage()));
     }
 }
