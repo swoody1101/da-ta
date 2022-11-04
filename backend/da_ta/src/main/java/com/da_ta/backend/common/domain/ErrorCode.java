c952945bc00dd6b69c33ab950e7c38ce7deb5278
swoody <swoody1101@gmail.com>
2022-11-04 13:48:00 +0900

    feat: 편지 수집 불가능 에러 메시지 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/common/domain/ErrorCode.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/common/domain/ErrorCode.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    COLLECT_BAD_REQUEST("편지를 수집할 수 없습니다."),
+
     BACKGROUND_NOT_FOUND("편지지를 찾을 수 없습니다."),
     FLOATED_LETTER_NOT_FOUND("바다에 띄워진 편지를 찾을 수 없습니다."),
     FONT_NOT_FOUND("폰트를 찾을 수 없습니다."),
