d6d1a52563e5eed2f7cd83c2e6165e71fa0baae3
giyoon27 <giyoon27@naver.com>
2022-11-04 11:11:20 +0900

    feat: 토큰 재발급 기능 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/account/user/controller/UserController.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/account/user/controller/UserController.java
@@ -3,15 +3,13 @@ package com.da_ta.backend.account.user.controller;
 import com.da_ta.backend.account.user.controller.dto.LoginRequest;
 import com.da_ta.backend.account.user.controller.dto.LoginResponse;
 import com.da_ta.backend.account.user.service.UserService;
+import com.da_ta.backend.common.domain.Message;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 public class UserController {
 
+    private final String AUTHORIZATION = "Authorization";
     private final UserService userService;
 
     @PostMapping("/login")
@@ -29,5 +28,15 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK)
                 .headers(headers)
                 .body(loginResponse);
+    }
+
+    @GetMapping("/reissue")
+    public ResponseEntity<Message> reissue(@RequestHeader(AUTHORIZATION) String token) {
+        HttpHeaders headers = new HttpHeaders();
+        Message message = userService.reissue(headers, token);
+        log.info("reissued access-token :" + headers);
+        return ResponseEntity.status(HttpStatus.OK)
+                .headers(headers)
+                .body(message);
     }
 }
