6b93fa4c7a53f1e4aba5af60bc7b1baf189473f9
giyoon27 <giyoon27@naver.com>
2022-11-04 11:12:58 +0900

    feat: 상수 및 변수 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/account/user/service/UserService.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/account/user/service/UserService.java
@@ -1,18 +1,48 @@
 package com.da_ta.backend.account.user.service;
 
-import com.da_ta.backend.account.user.controller.dto.LoginRequest;
-import com.da_ta.backend.account.user.controller.dto.LoginResponse;
+import com.da_ta.backend.account.jwt.JwtTokenProvider;
+import com.da_ta.backend.account.user.domain.repository.BanStatusRepository;
+import com.da_ta.backend.account.user.domain.repository.RedisRepository;
 import com.da_ta.backend.account.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final String GENERATE_NICKNAME_URL = "https://nickname.hwanmoo.kr/?format=json&count=1";
+    private final String CONTENT_TYPE = "Content-type";
+    private final String CONTENT_TYPE_VALUE = "application/x-www-form-urlencoded;charset=utf-8";
+    private final String GRANT_TYPE_VALUE = "authorization_code";
+    private final String TOKEN_SUBJECT = "sub";
+    private final String DELIMITER = " ";
     private final UserRepository userRepository;
+    private final BanStatusRepository banStatusRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RedisRepository redisRepository;
 
-    public LoginResponse login(LoginRequest loginRequest) {
-        return null;
-    }
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${kakao.client-secret}")
+    private String clientSecret;
+
+    @Value("${provider.kakao.token-uri}")
+    private String tokenUri;
+
+    @Value("${provider.kakao.user-info-uri}")
+    private String userInfoUri;
+
+    @Value("${spring.jwt.response.header}")
+    private String jwtHeader;
+
+    @Value("${spring.jwt.token.prefix}")
+    private String jwtTokenPrefix;
 }
