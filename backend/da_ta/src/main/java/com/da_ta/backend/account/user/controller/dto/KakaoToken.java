829e538edf88199337b744575b9ce4f0f9ebcdb1
giyoon27 <giyoon27@naver.com>
2022-11-06 17:30:05 +0900

    feat: 클래스 레벨 Jackson 바인딩 어노테이션 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/account/user/controller/dto/KakaoToken.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/account/user/controller/dto/KakaoToken.java
@@ -1,28 +1,21 @@
 package com.da_ta.backend.account.user.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoToken {
 
-    @JsonProperty(value = "access_token")
     private String accessToken;
-
-    @JsonProperty(value = "token_type")
     private String tokenType;
-
-    @JsonProperty(value = "refresh_token")
     private String refreshToken;
-
-    @JsonProperty(value = "expires_in")
     private int expiresIn;
-
     private String scope;
-
-    @JsonProperty(value = "refresh_token_expires_in")
     private int refreshTokenExpiresIn;
 }
