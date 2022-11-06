d5ad20b15de3878df76e5f99d7f2f6362d636805
giyoon27 <giyoon27@naver.com>
2022-11-06 17:28:44 +0900

    feat: 반환형 수정

--- backend/da_ta/src/main/java/com/da_ta/backend/account/user/domain/repository/UserRepository.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/account/user/domain/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.da_ta.backend.account.user.domain.repository;
 import com.da_ta.backend.account.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByKakaoId(String kakaoId);
+    Optional<User> findByKakaoId(String kakaoId);
 
     boolean existsByNickname(String randomNickname);
 }
