09d1bb72a3d1d7b86d4f0177899113f3185b760b
swoody <swoody1101@gmail.com>
2022-11-07 09:06:48 +0900

    feat: 답변 찾기 함수 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/ReplyRepository.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/ReplyRepository.java
@@ -1,7 +1,14 @@
 package com.da_ta.backend.letter.domain.repository;
 
+import com.da_ta.backend.letter.domain.entity.CollectedLetter;
 import com.da_ta.backend.letter.domain.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    boolean existsByIsReadTrueAndIsActiveTrueAndRecipientId(Long RecipientId);
+
+    List<Reply> findAllByUserIdAndIsActiveTrueOrderByCreatedDate(Long userId);
 }
