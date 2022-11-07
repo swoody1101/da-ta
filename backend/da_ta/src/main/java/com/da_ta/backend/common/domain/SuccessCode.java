fc429ca95a8dee36b9ba96c3244540d59ac827c6
swoody <swoody1101@gmail.com>
2022-11-07 16:06:06 +0900

    refactor: SuccessCode 수정

--- backend/da_ta/src/main/java/com/da_ta/backend/common/domain/SuccessCode.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/common/domain/SuccessCode.java
@@ -7,23 +7,20 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SuccessCode {
 
-    REISSUED_TOKEN("토큰이 재발급되었습니다."),
-    AGE_RANGE_UPDATED("연령대 수정이 완료되었습니다"),
     ALERT_OPTION_UPDATED("알림 활성 여부 수정이 완료되었습니다."),
+    AGE_RANGE_UPDATED("연령대 수정이 완료되었습니다"),
+    REISSUED_TOKEN("토큰이 재발급되었습니다."),
     USER_DELETED("회원 탈퇴가 완료되었습니다"),
 
-    COLLECTED_LETTER_CREATED("편지를 주웠습니다."),
     COLLECTED_LETTER_DELETED("편지가 보관함에서 삭제되었습니다."),
+    IMAGE_LETTER_FLOATED("이미지 편지를 바다에 띄웠습니다."),
+    LETTER_ACCUSED("편지 신고가 완료되었습니다."),
+    LETTER_COLLECTED("편지를 주웠습니다."),
+    LETTER_FLOATED_AGAIN("편지를 바다에 다시 띄웠습니다."),
     REPLY_DELETED("답장 받은 편지가 삭제되었습니다."),
-    FLOATED_LETTER_CREATED("편지를 바다에 다시 띄웠습니다."),
-    IMAGE_LETTER_CREATED("이미지 편지를 바다에 띄웠습니다."),
-    LETTER_ACCUSATION_CREATED("편지 신고가 완료되었습니다."),
-    REPLY_CREATED("답장을 발송하였습니다."),
-    REPLY_RECEPTION_CHECK_CREATED("답장 읽음 처리에 성공하였습니다."),
-    TEXT_LETTER_CREATED("텍스트 편지를 바다에 띄웠습니다."),
-
-    FLOATED_LETTER_NO_CONTENT("편지를 바다에 다시 띄웠습니다."),
-    REPLY_RECEPTION_CHECK_NO_CONTENT("답장 읽음 처리에 성공하였습니다.");
+    REPLY_RECEPTION_CHECKED("답장 읽음 처리에 성공하였습니다."),
+    REPLY_SENT("답장을 발송하였습니다."),
+    TEXT_LETTER_FLOATED("텍스트 편지를 바다에 띄웠습니다.");
 
     private final String message;
 }
