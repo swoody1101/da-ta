ce1e57506782d8ed3bbb25e5ac841e77b81020d2
mingyu <mingyu1218@naver.com>
2022-11-07 12:28:40 +0900

    feat: 편지지배경, 폰트 id로 api요청 변경

--- frontend/src/api/letterWriteAPI.js
+++ frontend/src/api/letterWriteAPI.js
@@ -4,48 +4,55 @@
 import { client } from "../utils/client";
 import { useRecoilValue } from "recoil";
 import { userState } from "../recoil/Atoms";
+import { LetterOptions } from "./../constants/Options";
 
 /**
  * @description 텍스트 편지 쓰기
  */
-export const saveTextLetter = async (options, userId) => {
-	const result = await client
-		.post(`/letters/1`, {
-			userId: userId,
-			option: {
-				replyOption: options.allowReply,
-				ageOption: options.age,
-			},
-			textLetterInfo: {
-				title: "",
-				content: "",
-				backgroundUrl: "",
-				fontName: "",
-			},
-		})
-		.then((response) => response)
-		.catch((error) => error);
-	return result;
+export const saveTextLetter = async (options, userId, title, content) => {
+  const body = {
+    userId: userId,
+    option: {
+      replyOption: options.allowReply,
+      ageOption: LetterOptions.AGES_VALUE[options.age],
+    },
+    textLetterInfo: {
+      title: title,
+      content: content,
+      backgroundId: LetterOptions.PAPERS.indexOf(options.paper),
+      fontId: LetterOptions.FONTS.indexOf(options.font),
+    },
+  };
+
+  console.log(body);
+
+  const result = await client
+    .post(`/letters/1`, body)
+    .then((response) => response)
+    .catch((error) => error);
+  return result;
 };
 
 /**
  * @description 그림 편지 쓰기
  */
 export const saveCanvasLetter = async (options, imageLetterUrl, userId) => {
-	const result = await client
-		.post(`/letters/2`, {
-			userId: userId,
-			option: {
-				replyOption: options.allowReply,
-				ageOption: options.age,
-			},
-			imageLetterInfo: {
-				title: "도화지 편지",
-				imageLetterUrl: imageLetterUrl,
-				backgroundUrl: options.paper,
-			},
-		})
-		.then((response) => response)
-		.catch((error) => error);
-	return result;
+  const body = {
+    userId: userId,
+    option: {
+      replyOption: options.allowReply,
+      ageOption: LetterOptions.AGES_VALUE[options.age],
+    },
+    imageLetterInfo: {
+      title: "도화지 편지",
+      imageLetterUrl: imageLetterUrl,
+      backgroundId: LetterOptions.PAPERS.indexOf(options.paper),
+    },
+  };
+
+  const result = await client
+    .post(`/letters/2`, body)
+    .then((response) => response)
+    .catch((error) => error);
+  return result;
 };
