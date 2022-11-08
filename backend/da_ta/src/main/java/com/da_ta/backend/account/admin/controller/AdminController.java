a1fdc2b7cb53c170e85d57f9511911ced47ef712
giyoon27 <giyoon27@naver.com>
2022-11-08 10:20:50 +0900

    feat: 사용자 권한 수정 기능 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/account/admin/controller/AdminController.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/account/admin/controller/AdminController.java
@@ -1,7 +1,9 @@
 package com.da_ta.backend.account.admin.controller;
 
 import com.da_ta.backend.account.admin.controller.dto.FindUsersResponse;
+import com.da_ta.backend.account.admin.controller.dto.UpdateRoleRequest;
 import com.da_ta.backend.account.admin.service.AdminService;
+import com.da_ta.backend.common.domain.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,5 +22,13 @@ public class AdminController {
     public ResponseEntity<FindUsersResponse> findUsers(@RequestHeader(AUTHORIZATION) String token) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(adminService.findUsers(token));
+    }
+
+    @PutMapping("/{user_id}")
+    public ResponseEntity<Message> updateRole(@RequestHeader(AUTHORIZATION) String token,
+                                              @PathVariable("user_id") Long userId,
+                                              @RequestBody UpdateRoleRequest updateRoleRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(adminService.updateRole(token, userId, updateRoleRequest));
     }
 }
