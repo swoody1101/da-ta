16e14fd58040d19624ee95ec6de49d13453d58ed
giyoon27 <giyoon27@naver.com>
2022-11-08 12:35:56 +0900

    feat: 마이페이지 호출 기능 추가

---
+++ backend/da_ta/src/main/java/com/da_ta/backend/account/user/controller/dto/MyPageResponse.java
@@ -0,0 +1,14 @@
+package com.da_ta.backend.account.user.controller.dto;
+
+import com.da_ta.backend.common.domain.Age;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MyPageResponse {
+
+    private Long userId;
+    private Age ageRange;
+    private boolean alertOption;
+}
