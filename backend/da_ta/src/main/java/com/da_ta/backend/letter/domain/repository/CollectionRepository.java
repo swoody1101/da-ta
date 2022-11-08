06a65e552bbf9e3f7dba9f294a7696111711778a
swoody <swoody1101@gmail.com>
2022-11-08 11:09:57 +0900

    refactor: 엔티티명 및 컬럼명 변경

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/CollectionRepository.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/CollectionRepository.java
@@ -1,14 +1,14 @@
 package com.da_ta.backend.letter.domain.repository;
 
-import com.da_ta.backend.letter.domain.entity.Collection;
+import com.da_ta.backend.letter.domain.entity.CollectedLetter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CollectionRepository extends JpaRepository<Collection, Long> {
+public interface CollectionRepository extends JpaRepository<CollectedLetter, Long> {
 
-    Optional<Collection> findByCollectedLetterIdAndUserId(Long collectedLetterId, Long userId);
+    Optional<CollectedLetter> findByLetterIdAndUserId(Long letterId, Long userId);
 
-    List<Collection> findAllByUserIdAndIsActiveTrueOrderByCreatedDateDesc(Long userId);
+    List<CollectedLetter> findAllByUserIdAndIsActiveTrueOrderByCreatedDateDesc(Long userId);
 }
