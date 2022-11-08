9373139115789491ef469ed5ed31ef1901c65631
SSAFY <tgb02087@naver.com>
2022-11-08 10:31:14 +0900

    fix: Redis 접근 버그수정

--- backend/da_ta/src/main/java/com/da_ta/backend/config/RedisConfig.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -17,9 +18,21 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
+    @Value("${spring.redis.username}")
+    private String redisusername;
+
+    @Value("${spring.redis.password}")
+    private String redisuserpassword;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setUsername(redisusername);
+        redisStandaloneConfiguration.setPassword(redisuserpassword);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory((redisStandaloneConfiguration));
+        return lettuceConnectionFactory;
     }
 
     @Bean
