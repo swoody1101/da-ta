eb8a5ca96b978b8b34524e65b71450d02ba93bf6
kby <tetepiti149@gmail.com>
2022-11-09 13:40:19 +0900

    refactor: 안쓰는 변수 삭제

--- frontend/src/pages/letter_read/LetterReadPage.jsx
+++ frontend/src/pages/letter_read/LetterReadPage.jsx
@@ -52,7 +52,4 @@ const ReadWrapper = styled(Wrapper)`
   align-items: center;
   justify-content: center;
 `;
-
-const memilggot =
-  "메밀꽃 필 무렵은 1936년 《조광》지에 발표된 이효석의 단편소설이다. 소설의 주 무대는 강원도 평창군 봉평면 일대이며, 마치 시처럼 서정적인 표현이 다수 사용되기도 했다. 원제는 《모밀꽃 필 무렵》이나, 현행 맞춤법 규정에 따라 표기한다";
 export default LetterReadPage;
