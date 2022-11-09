73371bf6a033cfd9acadf527f8ff7c1be2174cb6
giyoon27 <giyoon27@naver.com>
2022-11-09 13:01:50 +0900

    feat: 편지 신고 처리 여부 수정 메서드 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/LetterAccusation.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/LetterAccusation.java
@@ -27,4 +27,8 @@ public class LetterAccusation extends CommonEntity {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "letter_id")
     private Letter letter;
+
+    public void updateIsSolved() {
+        this.isSolved = true;
+    }
 }
