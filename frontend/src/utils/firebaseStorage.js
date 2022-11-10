ba88ccc97a5f5d9d1f7af5225722efdedecc25de
mingyu <mingyu1218@naver.com>
2022-11-10 10:56:07 +0900

    fix: firebase storage download 인자 수정

--- frontend/src/utils/firebaseStorage.js
+++ frontend/src/utils/firebaseStorage.js
@@ -10,15 +10,15 @@ const storage = firebaseStorage;
 
 /** dataURL을 File 객체로 변환해주는 함수 */
 const dataURLtoFile = (dataurl, filename) => {
-	var arr = dataurl.split(","),
-		mime = arr[0].match(/:(.*?);/)[1],
-		bstr = atob(arr[1]),
-		n = bstr.length,
-		u8arr = new Uint8Array(n);
-	while (n--) {
-		u8arr[n] = bstr.charCodeAt(n);
-	}
-	return new File([u8arr], filename, { type: mime });
+  var arr = dataurl.split(","),
+    mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new File([u8arr], filename, { type: mime });
 };
 
 /** firebase storage에 canvas 이미지 업로드
@@ -27,30 +27,29 @@ const dataURLtoFile = (dataurl, filename) => {
  * @return firebase storage에 저장된 경로 풀네임
  */
 export const uploadFirebaseStorage = (imageDataUrl, path) => {
-	const filePath = path ? path : "drawings/";
-	const filename = new Date().valueOf();
+  const filePath = path ? path : "drawings/";
+  const filename = new Date().valueOf();
 
-	const storageRef = storage.ref();
-	const storePath = storageRef.child(`${filePath}${filename}.png`); // firebase storage 내 저장할 경로
-	const imageFile = dataURLtoFile(imageDataUrl, "canvas_image.png");
+  const storageRef = storage.ref();
+  const storePath = storageRef.child(`${filePath}${filename}.png`); // firebase storage 내 저장할 경로
+  const imageFile = dataURLtoFile(imageDataUrl, "canvas_image.png");
 
-	storePath
-		.put(imageFile)
-		.then((result) => {
-			result;
-		})
-		.catch((error) => error);
+  storePath
+    .put(imageFile)
+    .then((result) => {
+      result;
+    })
+    .catch((error) => error);
 
-	return `${filePath}${filename}`;
+  return `${filePath}${filename}`;
 };
 
 /** firebase storage에 저장된 이미지 불러오기
- * @param {string} path 파일이름 이전까지의 firebase storage 경로
- * @param {string} filename firebase storage에 저장된 파일 이름
+ * @param {string} path 파일이름 이전까지의 firebase storage 경로 (예시 -> drawings/1216162.png)
  */
-export const downloadFirebaseStorage = async (path, filename) => {
-	const result = await getDownloadURL(ref(storage, `${path}${filename}`))
-		.then((url) => url)
-		.catch((error) => error);
-	return result;
+export const downloadFirebaseStorage = async (path) => {
+  const result = await getDownloadURL(ref(storage, `${path}`))
+    .then((url) => url)
+    .catch((error) => error);
+  return result;
 };
