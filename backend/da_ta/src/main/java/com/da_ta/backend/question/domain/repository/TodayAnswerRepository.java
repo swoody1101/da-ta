1881e0d744b7e7a3ed2df70c8975a168bc16e3cf
giyoon27 <giyoon27@naver.com>
2022-11-10 13:30:31 +0900

    feat: 오늘의 질문에 대한 답변 목록 조회 쿼리 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/question/domain/repository/TodayAnswerRepository.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/question/domain/repository/TodayAnswerRepository.java
@@ -3,5 +3,9 @@ package com.da_ta.backend.question.domain.repository;
 import com.da_ta.backend.question.domain.entity.TodayAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodayAnswerRepository extends JpaRepository<TodayAnswer, Long> {
+
+    List<TodayAnswer> findAllByTodayQuestionId(Long questionId);
 }
