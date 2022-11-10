5ca87ff486676f9d151628b3ed78b06cc368adfe
giyoon27 <giyoon27@naver.com>
2022-11-10 10:20:28 +0900

    feat: 오늘의 질문 목록 조회 쿼리 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/question/domain/repository/TodayQuestionRepository.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/question/domain/repository/TodayQuestionRepository.java
@@ -2,11 +2,19 @@ package com.da_ta.backend.question.domain.repository;
 
 import com.da_ta.backend.question.domain.entity.TodayQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface TodayQuestionRepository extends JpaRepository<TodayQuestion, Long> {
 
     Optional<TodayQuestion> findByDate(LocalDate date);
+
+    @Query(value = "select * " +
+            "from today_question " +
+            "where date_format(date, '%Y-%m') = :date", nativeQuery = true)
+    List<TodayQuestion> findTodayQuestionsByYearAndMonth(@Param("date") String date);
 }
