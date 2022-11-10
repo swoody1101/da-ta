de6c149bce0952c6ba870595a1f75cdd867832c3
mingyu <mingyu1218@naver.com>
2022-11-10 12:36:23 +0900

    feat: notfound 페이지 이미지 추가

--- frontend/src/pages/error/NotFound.jsx
+++ frontend/src/pages/error/NotFound.jsx
@@ -4,33 +4,41 @@ import styled from "styled-components";
 import Title from "./../../components/atoms/Title";
 import Button from "./../../components/atoms/Button";
 import { MainText } from "./../../components/atoms/Text";
-import { RowCenterWrapper } from "./../../styles/Wrapper";
+import { AdminWrapper, RowCenterWrapper } from "./../../styles/Wrapper";
 
 const NotFound = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	return (
-		<RowCenterWrapper>
-			<Container>
-				<Title fontSize="2rem">Oops...!</Title>
-				<MainText margin="1rem 0 0 0">페이지를 찾을 수 없습니다.</MainText>
-				<Button margin="2rem 0 0 0" width="6rem" height="4rem" shadow={true} onClick={() => navigate("/")}>
-					홈
-				</Button>
-			</Container>
-		</RowCenterWrapper>
-	);
+  return (
+    <Container>
+      <img
+        src={`${process.env.PUBLIC_URL}/assets/images/common/not_found.png`}
+        width="120rem"
+      />
+      <Title fontSize="2rem">Oops...!</Title>
+      <MainText margin="1rem 0 0 0">페이지를 찾을 수 없습니다.</MainText>
+      <Button
+        margin="2rem 0 0 0"
+        width="6rem"
+        height="4rem"
+        shadow={true}
+        onClick={() => navigate("/")}
+      >
+        홈
+      </Button>
+    </Container>
+  );
 };
 
 const Container = styled.div`
-	display: flex;
-	width: 100%;
-	height: 100%;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	background-color: black;
-	color: white;
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+  color: white;
 `;
 
 export default NotFound;
