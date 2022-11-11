52f281364fa047c55bb61aaac27045a829085520
mingyu <mingyu1218@naver.com>
2022-11-11 12:46:28 +0900

    feat: pc 헤더 버튼 변경, 관리자페이지 버그 수정

--- frontend/src/pages/admin/Reports.jsx
+++ frontend/src/pages/admin/Reports.jsx
@@ -6,104 +6,135 @@ import { MainSmallText } from "./../../components/atoms/Text";
 import { useSetRecoilState } from "recoil";
 import { loadingState } from "../../recoil/Atoms";
 import { accuseUser, getReportList } from "./../../api/adminAPI";
-import { popWarningAlert, popSuccessAlert, popErrorAlert } from "./../../utils/sweetAlert";
+import {
+  popWarningAlert,
+  popSuccessAlert,
+  popErrorAlert,
+} from "./../../utils/sweetAlert";
 import ReportsTemplate from "../../components/templates/admin/ReportsTemplate";
 import { ModalContentBox, ModalContentWrapper } from "../../styles/Admin";
 
 const Reports = () => {
-	const [searchParams, setSearchParams] = useSearchParams();
-	const listType = searchParams.get("listType");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const listType = searchParams.get("listType");
 
-	const flexRate = [2, 4, 5, 5, 4, 2, 2];
-	const categoryList = ["번호", "신고 날짜", "신고자", "신고 대상", "신고 사유", "내용", "신고 처리"];
+  const flexRate = [2, 4, 5, 5, 4, 2, 2];
+  const categoryList = [
+    "번호",
+    "신고 날짜",
+    "신고자",
+    "신고 대상",
+    "신고 사유",
+    "내용",
+    "신고 처리",
+  ];
 
-	const [modalToggle, setModalToggle] = useState(false);
-	const [modalContent, setModalContent] = useState({
-		reportedNickname: null,
-		content: null, // 편지텍스트 또는 이미지url
-	});
-	const setLoading = useSetRecoilState(loadingState);
-	const [itemList, setItemList] = useState([]);
+  const [modalToggle, setModalToggle] = useState(false);
+  const [modalContent, setModalContent] = useState({
+    reportedNickname: null,
+    content: null, // 편지텍스트 또는 이미지url
+  });
+  const setLoading = useSetRecoilState(loadingState);
+  const [itemList, setItemList] = useState([]);
 
-	useEffect(() => {
-		initList();
-	}, [listType]);
+  useEffect(() => {
+    initList();
+  }, [listType]);
 
-	/** 신고 목록 초기화 */
-	const initList = async () => {
-		setLoading(true);
-		setItemList([]);
-		const response = await getReportList(listType);
-		setLoading(false);
-		if (response.status !== 200) {
-			popErrorAlert("목록 불러올 수 없음", "신고 목록을 불러오던 중 문제가 발생했습니다.");
-			return;
-		}
+  /** 신고 목록 초기화 */
+  const initList = async () => {
+    setLoading(true);
+    setItemList([]);
+    const response = await getReportList(listType);
+    setLoading(false);
+    if (response.status !== 200) {
+      popErrorAlert(
+        "목록 불러올 수 없음",
+        "신고 목록을 불러오던 중 문제가 발생했습니다."
+      );
+      return;
+    }
 
-		setItemList([...(listType === "letter" ? response.data.accusedLetters : response.data.accusedAnswers)]);
-	};
+    setItemList([
+      ...(listType === "letter"
+        ? response.data.accusedLetters
+        : response.data.accusedAnswers),
+    ]);
+  };
 
-	/** [편지, 오늘의질문 답변] 내용 보기 */
-	const handleModal = (item) => {
-		setModalContent({
-			reportedNickname: item.reportedNickname,
-			content: item.content,
-		});
-		setModalToggle(true);
-	};
+  /** [편지, 오늘의질문 답변] 내용 보기 */
+  const handleModal = (item) => {
+    setModalContent({
+      reportedNickname: item.reportedNickname,
+      content: item.content,
+    });
+    setModalToggle(true);
+  };
 
-	/** [편지, 오늘의질문 답변]신고 처리 */
-	const handleAccuse = async (item, letterAccusationId, e) => {
-		if (item.isSolved) {
-			popWarningAlert("신고 처리 실패", "이미 경고처리된 신고입니다.");
-			return;
-		}
+  /** [편지, 오늘의질문 답변]신고 처리 */
+  const handleAccuse = async (item, e) => {
+    if (item.isSolved) {
+      popWarningAlert("신고 처리 실패", "이미 경고처리된 신고입니다.");
+      return;
+    }
 
-		setLoading(true);
-		const response = await accuseUser(letterAccusationId, listType);
-		setLoading(false);
+    setLoading(true);
+    const response = await accuseUser(
+      listType === "letter" ? item.accusedLetterId : item.answerAccusationId,
+      listType
+    );
+    setLoading(false);
 
-		if (response.status !== 200 && response.status !== 201) {
-			popWarningAlert("신고 처리 실패", "신고 처리 중 문제가 발생했습니다.");
-			return;
-		}
+    if (response.status !== 200 && response.status !== 201) {
+      popWarningAlert("신고 처리 실패", "신고 처리 중 문제가 발생했습니다.");
+      return;
+    }
 
-		popSuccessAlert("신고 처리 성공", "신고에 대한 경고 처리가 완료되었습니다.");
-		initList();
-	};
+    popSuccessAlert(
+      "신고 처리 성공",
+      "신고에 대한 경고 처리가 완료되었습니다."
+    );
+    initList();
+  };
 
-	return (
-		<>
-			{modalToggle && (
-				<Modal titleText={"내용"} modalToggle={modalToggle} setModalToggle={setModalToggle}>
-					<ModalContentWrapper>
-						<MainSmallText color="black" fontWeight="bold">
-							신고 대상
-						</MainSmallText>
-						<ModalContentBox width="12rem" margin="1rem 0 0 0">
-							{modalContent.reportedNickname}
-						</ModalContentBox>
-						<MainSmallText color="black" fontWeight="bold" margin="2rem 0 0 0">
-							내용
-						</MainSmallText>
-						<ModalContentBox margin="1rem 0 0 0" width="80%" height="10rem">
-							{modalContent.content}
-						</ModalContentBox>
-					</ModalContentWrapper>
-				</Modal>
-			)}
+  return (
+    <>
+      {modalToggle && (
+        <Modal
+          titleText={"내용"}
+          modalToggle={modalToggle}
+          setModalToggle={setModalToggle}
+        >
+          <ModalContentWrapper>
+            <MainSmallText color="black" fontWeight="bold">
+              신고 대상
+            </MainSmallText>
+            <ModalContentBox width="12rem" margin="1rem 0 0 0">
+              {modalContent.reportedNickname}
+            </ModalContentBox>
+            <MainSmallText color="black" fontWeight="bold" margin="2rem 0 0 0">
+              내용
+            </MainSmallText>
+            <ModalContentBox margin="1rem 0 0 0" width="80%" height="10rem">
+              {modalContent.content}
+            </ModalContentBox>
+          </ModalContentWrapper>
+        </Modal>
+      )}
 
-			<ReportsTemplate
-				listType={listType}
-				title={listType === "letter" ? "신고 관리 - 편지" : "신고 관리 - 오늘의 답변"}
-				categoryList={categoryList}
-				itemList={itemList}
-				handleModal={handleModal}
-				handleAccuse={handleAccuse}
-				flexRate={flexRate}
-			/>
-		</>
-	);
+      <ReportsTemplate
+        listType={listType}
+        title={
+          listType === "letter" ? "신고 관리 - 편지" : "신고 관리 - 오늘의 답변"
+        }
+        categoryList={categoryList}
+        itemList={itemList}
+        handleModal={handleModal}
+        handleAccuse={handleAccuse}
+        flexRate={flexRate}
+      />
+    </>
+  );
 };
 
 export default Reports;
--- frontend/src/utils/client.js
+++ frontend/src/utils/client.js
@@ -1,83 +1,63 @@
 import axios from "axios";
 import { reissue } from "../api/authAPI";
 import { popErrorAlert } from "./sweetAlert";
+import { useCookies, Cookies } from "react-cookie";
 
 const BASE_URL = process.env.REACT_APP_REST_API_DOMAIN;
 // const BASE_URL = "http://localhost:8080/api/v1";
 
 export const client = axios.create({
-	baseURL: BASE_URL,
-	timeout: 5000,
+  baseURL: BASE_URL,
+  timeout: 5000,
 });
 
 /**
  * 요청 시 인터셉터
  */
 client.interceptors.request.use(
-	(config) => {
-		const accessToken = sessionStorage.getItem("ACCESS_TOKEN");
-		// const refreshToken = sessionStorage.getItem("REFRESH_TOKEN");
+  (config) => {
+    const accessToken = sessionStorage.getItem("ACCESS_TOKEN");
 
-		if (!accessToken) config.headers["Authorization"] = null;
-		else config.headers["Authorization"] = `Bearer ${accessToken}`;
+    if (!accessToken) {
+      config.headers["Authorization"] = null;
+    } else {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
 
-		return config;
-	},
-	(error) => {
-		console.log(error); // error
-		return Promise.reject(error);
-	}
+    return config;
+  },
+  (error) => {
+    console.log(error); // error
+    return Promise.reject(error);
+  }
 );
 
 /**
  * 응답 시 인터셉터
  */
 client.interceptors.response.use(
-	(config) => {
-		if (!config) {
-		}
+  (config) => {
+    if (!config) {
+    }
 
-		return config;
-	},
-	async (error) => {
-		const {
-			config,
-			code,
-			response: { status },
-		} = error;
+    return config;
+  },
+  async (error) => {
+    const {
+      config,
+      code,
+      response: { status },
+    } = error;
 
-		/** 서버와 연결이 끊어졌을 시 */
-		if (code === "ERR_NETWORK") {
-			popErrorAlert("네트워크 연결 오류", "서버와의 연결이 원활하지 않습니다. 네트워크 상태를 확인하시거나, 잠시 후 다시 시도해주세요.");
-			return Promise.reject(error);
-		}
+    /** 서버와 연결이 끊어졌을 시 */
+    if (code === "ERR_NETWORK") {
+      popErrorAlert(
+        "네트워크 연결 오류",
+        "서버와의 연결이 원활하지 않습니다. 네트워크 상태를 확인하시거나, 잠시 후 다시 시도해주세요."
+      );
+      return Promise.reject(error);
+    }
 
-		/** access token 유효기간 만료 시 */
-		if (response && status === 500) {
-			// try {
-			// 	const originalRequest = error.config;
-			// 	const data = await reissue();
-			// 	if (data) {
-			// 		const { accessToken, refreshToken } = data.data;
-			// 		localStorage.removeItem("user");
-			// 		localStorage.setItem("user", JSON.stringify(data.data, ["accessToken", "refreshToken"]));
-			// 		originalRequest.headers["accessToken"] = accessToken;
-			// 		originalRequest.headers["refreshToken"] = refreshToken;
-			// 		return await client.request(originalRequest);
-			// 	}
-			// } catch (error) {
-			// 	localStorage.removeItem("user");
-			// 	console.log(error);
-			// }
-			// return Promise.reject(error);
-		}
-
-		const isJWT = error.response.data.message.substr(0, 3) === "JWT";
-		if (isJWT) {
-			// 리프레싵 토큰 재발급 로직
-			console.log("리프레시 토큰");
-		}
-
-		return Promise.reject(error);
-	}
+    return Promise.reject(error);
+  }
 );
