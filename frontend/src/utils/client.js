2ae3a5505827d67f897a2ee2967520f1b79ecb91
mingyu <mingyu1218@naver.com>
2022-11-12 21:37:33 +0900

    fix: 리프레시 토큰 재발급 관련 interceptor 오류 해결 (axios 버전 1.1.3 -> 0.27.2 다운그레이드)

--- frontend/src/utils/client.js
+++ frontend/src/utils/client.js
@@ -7,80 +7,65 @@ const BASE_URL = process.env.REACT_APP_REST_API_DOMAIN;
 // const BASE_URL = "http://localhost:8080/api/v1";
 
 export const client = axios.create({
-  baseURL: BASE_URL,
-  timeout: 5000,
+	baseURL: BASE_URL,
+	// timeout: 5000,
 });
 
 /**
  * 요청 시 인터셉터
  */
 client.interceptors.request.use(
-  (config) => {
-    const accessToken = sessionStorage.getItem("ACCESS_TOKEN");
+	(config) => {
+		const accessToken = sessionStorage.getItem("ACCESS_TOKEN");
 
-    if (!accessToken) {
-      config.headers["Authorization"] = null;
-    } else {
-      config.headers["Authorization"] = `Bearer ${accessToken}`;
-    }
-    console.log(config.headers);
+		if (!accessToken) {
+			config.headers["Authorization"] = null;
+		} else {
+			config.headers["Authorization"] = `Bearer ${accessToken}`;
+		}
 
-    return config;
-  },
-  (error) => {
-    console.log(error); // error
-    return Promise.reject(error);
-  }
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
 );
 
 /**
  * 응답 시 인터셉터
  */
 client.interceptors.response.use(
-  (config) => {
-    console.log("성공!");
-    console.log(config);
-    return config;
-  },
-  async (error) => {
-    /** 서버와 연결이 끊어졌을 시 */
-    console.log(error);
-    if (error.code === "ERR_NETWORK") {
-      popErrorAlert(
-        "네트워크 연결 오류",
-        "서버와의 연결이 원활하지 않습니다. 네트워크 상태를 확인하시거나, 잠시 후 다시 시도해주세요."
-      );
-      return Promise.reject(error);
-    }
+	(config) => {
+		return config;
+	},
+	async (error) => {
+		/** 서버와 연결이 끊어졌을 시 */
+		if (error.code === "ERR_NETWORK") {
+			popErrorAlert("네트워크 연결 오류", "서버와의 연결이 원활하지 않습니다. 네트워크 상태를 확인하시거나, 잠시 후 다시 시도해주세요.");
+			return Promise.reject(error);
+		}
 
-    /** Access Token이 만료되었을 시 */
-    if (
-      error.response &&
-      (error.response.status === 401 || error.response.status === 500)
-    ) {
-      // 1. reissue 요청으로 refresh token 재발급 요청을 한다.
-      const response = await reissue();
-      if (!response || response.status !== 200) {
-        popErrorAlert("오류 발생", "예기치 못한 오류가 발생했습니다.");
-        localStorage.removeItem("recoil-persist");
-        window.location.href = "/";
-        return;
-      }
+		/** Access Token이 만료되었을 시 */
+		if (error.response.status === 401) {
+			// // 1. reissue 요청으로 refresh token 재발급 요청을 한다.
+			const response = await reissue();
 
-      // 2. 요청 후 받은 응답에서 access token을 추출해 localstorage에 저장한다.
-      const accessToken = response.headers["authorization"];
-      sessionStorage.setItem("ACCESS_TOKEN", accessToken);
+			if (!response || response.status !== 200) {
+				popErrorAlert("오류 발생", "예기치 못한 오류가 발생했습니다.");
+				localStorage.removeItem("recoil-persist");
+				window.location.href = "/";
+				return;
+			}
 
-      // 3. 못보냈던 config를 재전송한다.
-      const originalRequest = error.config;
-      console.log(originalRequest.headers);
-      originalRequest.headers["Authorization"] = `Bearer ${accessToken}`;
-      originalRequest.headers["Symbol"].Accept =
-        "application/json, text/plain, */*";
-      return await axios.request(originalRequest);
-      // return await client.request(originalRequest);
-    }
+			// // 2. 요청 후 받은 응답에서 access token을 추출해 localstorage에 저장한다.
+			const accessToken = response.headers["authorization"];
+			sessionStorage.setItem("ACCESS_TOKEN", accessToken);
 
-    return Promise.reject(error);
-  }
+			// 3. 못보냈던 config를 재전송한다.
+			error.config.headers.Authorization = `Bearer ${accessToken}`;
+
+			return await client(error.config);
+		}
+		return Promise.reject(error);
+	}
 );
