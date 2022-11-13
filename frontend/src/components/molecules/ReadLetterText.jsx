91b00f0d7f9ad8e694562451476aea4f1295f6bb
mingyu <mingyu1218@naver.com>
2022-11-14 01:57:02 +0900

    feat: ReadLetterText.jsx 줄바꿈 적용

--- frontend/src/components/molecules/ReadLetterText.jsx
+++ frontend/src/components/molecules/ReadLetterText.jsx
@@ -8,123 +8,122 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 import { useSetRecoilState } from "recoil";
 import { reportModalState } from "../../recoil/Atoms";
+import { useEffect, useState } from "react";
 
 const ReadLetterText = ({ info, nickname }) => {
-  const setReportModal = useSetRecoilState(reportModalState);
+	const setReportModal = useSetRecoilState(reportModalState);
+	const [showContent, setShowContent] = useState("");
 
-  const DateToString = (writtenDate) => {
-    const ToDate = new Date(writtenDate);
-    return `${ToDate.getFullYear()}년 ${
-      ToDate.getMonth() + 1
-    }월 ${ToDate.getDate()}일`;
-  };
+	const DateToString = (writtenDate) => {
+		const ToDate = new Date(writtenDate);
+		return `${ToDate.getFullYear()}년 ${ToDate.getMonth() + 1}월 ${ToDate.getDate()}일`;
+	};
 
-  return (
-    <ContentBlock
-      height={SizeTypes.PC_LETTER_HEIGHT}
-      mWidth={SizeTypes.MOBILE_LETTER_WIDTH}
-      mHeight={SizeTypes.MOBILE_LETTER_HEIGHT}
-      flexDirection="column"
-      optionToggle={false}
-    >
-      <LetterImg
-        src={`${process.env.PUBLIC_URL}/assets/images/letter/${
-          LetterOptions.PAPERS[info.backgroundId]
-        }.png`}
-      />
-      <Container
-        width="96%"
-        height={SizeTypes.PC_TITLE_HEIGHT}
-        padding="0.5rem 0 0 0"
-      >
-        <LetterTitle width="96%" fontSize="1.2rem" fontWeight="bold">
-          {info.title}
-          <IconReportBtn>
-            <FontAwesomeIcon
-              icon={faTriangleExclamation}
-              style={{
-                color: "#F44336",
-                cursor: "pointer",
-              }}
-              size="lg"
-              onClick={() => {
-                setReportModal(true);
-              }}
-            />
-          </IconReportBtn>
-        </LetterTitle>
-      </Container>
-      <Container
-        width="96%"
-        height={SizeTypes.PC_TITLE_HEIGHT}
-        padding="0 0 0.5rem 0"
-      >
-        <LetterTitle width="96%" fontSize="1rem" fontWeight="bold">
-          {`${DateToString(info.writtenDate)}, ${nickname}`}
-        </LetterTitle>
-      </Container>
-      <LetterContent fontFamily={LetterOptions.FONTS[info.fontId]}>
-        {info.content}
-      </LetterContent>
-    </ContentBlock>
-  );
+	useEffect(() => {
+		setShowContent(info.content.replaceAll("\n", "<br/>"));
+	}, []);
+
+	return (
+		<ContentBlock
+			height={SizeTypes.PC_LETTER_HEIGHT}
+			mWidth={SizeTypes.MOBILE_LETTER_WIDTH}
+			mHeight={SizeTypes.MOBILE_LETTER_HEIGHT}
+			flexDirection="column"
+			optionToggle={false}
+		>
+			<LetterImg src={`${process.env.PUBLIC_URL}/assets/images/letter/${LetterOptions.PAPERS[info.backgroundId]}.png`} />
+			<Container width="96%" height={SizeTypes.PC_TITLE_HEIGHT} padding="0.5rem 0 0 0">
+				<LetterTitle width="96%" fontSize="1.2rem" fontWeight="bold">
+					{info.title}
+					<IconReportBtn>
+						<FontAwesomeIcon
+							icon={faTriangleExclamation}
+							style={{
+								color: "#F44336",
+								cursor: "pointer",
+							}}
+							size="lg"
+							onClick={() => {
+								setReportModal(true);
+							}}
+						/>
+					</IconReportBtn>
+				</LetterTitle>
+			</Container>
+			<Container width="96%" height={SizeTypes.PC_TITLE_HEIGHT} padding="0 0 0.5rem 0">
+				<LetterTitle width="96%" fontSize="1rem" fontWeight="bold">
+					{`${DateToString(info.writtenDate)}, ${nickname}`}
+				</LetterTitle>
+			</Container>
+			<LetterContent fontFamily={LetterOptions.FONTS[info.fontId]}>
+				{info.content.split("\n").map((line, index) => {
+					return (
+						<>
+							{line}
+							<br />
+						</>
+					);
+				})}
+			</LetterContent>
+		</ContentBlock>
+	);
 };
 
 const Container = styled.div`
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
-  padding: ${(props) => props.padding};
-  align-items: center;
-  justify-content: center;
-  position: relative;
+	width: ${(props) => props.width};
+	height: ${(props) => props.height};
+	padding: ${(props) => props.padding};
+	align-items: center;
+	justify-content: center;
+	position: relative;
 `;
 
 const LetterTitle = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  text-align: left;
-  font-size: ${(props) => props.fontSize};
-  font-weight: ${(props) => props.fontWeight};
-  padding: 0.6rem 0.4rem;
-  transition: 0.2s ease;
-  ${media.phone`
+	display: flex;
+	width: 100%;
+	height: 100%;
+	text-align: left;
+	font-size: ${(props) => props.fontSize};
+	font-weight: ${(props) => props.fontWeight};
+	padding: 0.6rem 0.4rem;
+	transition: 0.2s ease;
+	${media.phone`
     width: ${(props) => (props.mWidth ? props.mWidth : props.width)};
   `}
 `;
 
 LetterTitle.defaultProps = {
-  width: "16rem",
-  background: "none",
-  fontSize: "1rem",
+	width: "16rem",
+	background: "none",
+	fontSize: "1rem",
 };
 
 const LetterContent = styled.div`
-  display: flex;
-  resize: none;
-  border: none;
-  text-align: left;
-  z-index: 10;
-  width: 100%;
-  height: ${SizeTypes.PC_CONTENT_HEIGHT};
-  padding: 1rem;
-  box-sizing: border-box;
-  background: transparent;
-  font-family: ${(props) => props.fontFamily || ""};
-  font-size: 1rem;
-  line-height: 1.5rem;
-  color: black;
-  overflow-y: auto;
+	display: flex;
+	resize: none;
+	border: none;
+	text-align: left;
+	z-index: 10;
+	width: 100%;
+	height: ${SizeTypes.PC_CONTENT_HEIGHT};
+	padding: 1rem;
+	box-sizing: border-box;
+	background: transparent;
+	font-family: ${(props) => props.fontFamily || ""};
+	font-size: 1rem;
+	line-height: 1.5rem;
+	color: black;
+	overflow-y: auto;
 
-  ${media.phone`
+	${media.phone`
     height: ${SizeTypes.MOBILE_CONTENT_HEIGHT};
   `}
 `;
 
 const IconReportBtn = styled.div`
-  margin-right: 15px;
-  display: none;
-  ${media.phone`
+	margin-right: 15px;
+	display: none;
+	${media.phone`
     display: block;
   `}
 `;
