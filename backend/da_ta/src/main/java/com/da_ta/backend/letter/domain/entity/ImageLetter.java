14a25955e9b5cb15cbaa0538a0759cc5ccc4a61d
swoody <swoody1101@gmail.com>
2022-11-14 16:53:03 +0900

    refactor: 생성자 자동 생성 어노테이션 추가

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/ImageLetter.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/ImageLetter.java
@@ -1,6 +1,7 @@
 package com.da_ta.backend.letter.domain.entity;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class ImageLetter extends Letter {
 
