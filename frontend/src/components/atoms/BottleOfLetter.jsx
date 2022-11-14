a2737cd05d48232268a3fd77c61d8215c5d5bd74
mingyu <mingyu1218@naver.com>
2022-11-15 00:53:39 +0900

    fix: 병 위치 수정

--- frontend/src/components/atoms/BottleOfLetter.jsx
+++ frontend/src/components/atoms/BottleOfLetter.jsx
@@ -9,6 +9,21 @@ import { media } from "../../utils/styleUtil";
  * @param path 물병 사진 경로
  */
 
+const BottleOfLetter = ({ path, ...props }) => {
+	return (
+		<BG {...props}>
+			<img src={path} width="100%" height="100%" />
+		</BG>
+	);
+};
+
+BottleOfLetter.defaultProps = {
+	path: `${process.env.PUBLIC_URL}/assets/images/common/bottle_of_letter_btn.png`,
+	width: "100%",
+	height: "auto",
+	isRight: "1",
+};
+
 const moveBottle = keyframes`
   0% {
     margin-top: 0px;
@@ -16,39 +31,21 @@ const moveBottle = keyframes`
   100% {
     margin-top: 20px;
   }
-  `;
-
-const BottleOfLetter = ({ path }) => {
-	return (
-		<BG>
-			<img src={path}></img>
-		</BG>
-	);
-};
-
-BottleOfLetter.defaultProps = {
-	path: `${process.env.PUBLIC_URL}/assets/images/common/bottle_of_letter_btn.png`,
-	width: "10%",
-	height: "10%",
-	isRight: "1",
-};
+`;
 
 const BG = styled.div`
-	width: ${(props) => props.width};
-	height: ${(props) => props.height};
+	width: ${(props) => props.width || "100%"};
+	height: ${(props) => props.height || "auto"};
+	align-items: center;
+	justify-content: center;
 	overflow: hidden;
-	position: absolute;
+	position: ${(props) => props.position || "absolute"};
 	z-index: 1.5;
-	animation: ${(props) => (props.isRight ? moveBottle : moveBottle)} 1.5s linear 0s infinite alternate;
-	top: 2rem;
-	left: 50%;
-	transform: translate(-50%, -50%);
+	animation: ${moveBottle} 1.5s linear 0s infinite alternate;
 
 	${media.phone`
-		top: -5rem;
-		transform: translate(-50%, 0%);
-  	width: ${(props) => props.mWidth};
-  	height: ${(props) => props.mHeight};
+  	width: ${(props) => props.mWidth || "80%"};
+  	height: ${(props) => props.mHeight || "auto"};
 	`}
 `;
 
