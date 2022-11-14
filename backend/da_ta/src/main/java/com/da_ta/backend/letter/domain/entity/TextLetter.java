dc48482b78b4471147423220b5dcdd0aa8ae3fe9
swoody <swoody1101@gmail.com>
2022-11-14 10:13:36 +0900

    feat: content 자료형 LONGTEXT로 변경

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/TextLetter.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/entity/TextLetter.java
@@ -15,6 +15,7 @@ import javax.validation.constraints.NotNull;
 public class TextLetter extends Letter {
 
     @NotNull
+    @Column(columnDefinition = "LONGTEXT")
     private String content;
 
     @NotNull
