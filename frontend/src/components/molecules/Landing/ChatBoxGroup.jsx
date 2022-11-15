22414146c9954111854c54fb9d1bc9960d41684c
mingyu <mingyu1218@naver.com>
2022-11-16 08:21:47 +0900

    feat: 오늘의 답변 적용, 캐로셀 구성

--- frontend/src/components/molecules/Landing/ChatBoxGroup.jsx
+++ frontend/src/components/molecules/Landing/ChatBoxGroup.jsx
@@ -9,142 +9,115 @@ import { MainTestText } from "../../atoms/Text";
 import { loginState } from "../../../recoil/Atoms";
 import { popWarningAlert } from "../../../utils/sweetAlert";
 import { useRecoilValue } from "recoil";
-import { getTodayQuestion } from "../../../api/questionReadAPI";
 
-const ChatBoxGroup = ({
-  children,
-  onClick,
-  handleModalA,
-  handleModalB,
-  ...props
-}) => {
-  {
-    const isLogin = useRecoilValue(loginState);
-    const [todayQuestionQ, setTodayQuestionQ] = useState([]); //변하는 오늘의 질문
+const ChatBoxGroup = ({ handleModalA, handleModalB, todayQuestion, ...props }) => {
+	const isLogin = useRecoilValue(loginState);
 
-    const handlerClickModalA = () => {
-      return isLogin
-        ? handleModalA()
-        : { ...popWarningAlert("", "로그인 후 이용해주세요.") };
-    };
+	const handlerClickModalA = () => {
+		return isLogin ? handleModalA() : { ...popWarningAlert("", "로그인 후 이용해주세요.") };
+	};
 
-    const handlerClickModalB = () => {
-      return isLogin
-        ? handleModalB()
-        : { ...popWarningAlert("", "로그인 후 이용해주세요.") };
-    };
+	const handlerClickModalB = () => {
+		return isLogin ? handleModalB() : { ...popWarningAlert("", "로그인 후 이용해주세요.") };
+	};
 
-    // 오늘의 질문 가져오는 api용 1
-    useEffect(() => {
-      mainGetQuestion();
-    }, []);
+	// 오늘의 질문 가져오는 api용 1
+	useEffect(() => {}, []);
 
-    // 오늘의 질문 가져오는 api용 2
-    const mainGetQuestion = async () => {
-      let response = await getTodayQuestion();
-      const todayQuestion = response.data;
-      setTodayQuestionQ(todayQuestion);
-    };
+	return (
+		<>
+			<ChatBoxMolecure {...props}>
+				<TextBox>
+					<MainTestText fontWeight="500">{todayQuestion}</MainTestText>
+				</TextBox>
+				<ButtonBox>
+					<Button
+						fontSize="1.1rem"
+						height="3rem"
+						width="9rem"
+						margin="0 2.5% 0 2.5%"
+						shadow={true}
+						color="#5F0EB0"
+						borderStyle="2px solid #5F0EB0"
+						hasBorder={false}
+						hoverBgOpacity="0.25"
+						onClick={() => handlerClickModalA()}
+					>
+						답변하기
+					</Button>
 
-    return (
-      <>
-        <ChatBoxMolecure {...props} onClick={onClick}>
-          {children}
-          <TextBox>
-            <MainTestText fontWeight="500">
-              {todayQuestionQ.question}
-            </MainTestText>
-          </TextBox>
-
-          <ButtonBox>
-            <Button
-              fontSize="1.2rem"
-              height="3rem"
-              width="9rem"
-              margin="1% 0 0 2%"
-              shadow={true}
-              color="#5F0EB0"
-              borderStyle="2px solid #5F0EB0"
-              hasBorder={false}
-              hoverBgOpacity="0.25"
-              onClick={() => handlerClickModalA()}
-            >
-              답변하기
-            </Button>
-
-            <Button
-              fontSize="1.2rem"
-              height="3rem"
-              width="9rem"
-              margin="1% 2% 0 9%"
-              shadow={true}
-              color="#5F0EB0"
-              borderStyle="2px solid #5F0EB0"
-              hasBorder={false}
-              hoverBgOpacity="0.25"
-              onClick={() => handlerClickModalB()}
-            >
-              다른 답변보기
-            </Button>
-          </ButtonBox>
-        </ChatBoxMolecure>
-      </>
-    );
-  }
+					<Button
+						fontSize="1.1rem"
+						height="3rem"
+						width="9rem"
+						margin="0 2.5% 0 2.5%"
+						shadow={true}
+						color="#5F0EB0"
+						borderStyle="2px solid #5F0EB0"
+						hasBorder={false}
+						hoverBgOpacity="0.25"
+						onClick={() => handlerClickModalB()}
+					>
+						다른 답변보기
+					</Button>
+				</ButtonBox>
+			</ChatBoxMolecure>
+		</>
+	);
 };
 
 const ChatBoxMolecure = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: absolute;
-  top: 30%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  align-items: center;
-  justify-content: center;
-  width: 440px;
-  height: 200px;
-  padding: 0.1rem;
-  background: #ffffff;
-  border-radius: 16px;
+	display: flex;
+	flex-direction: column;
+	position: absolute;
+	top: 30%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	align-items: center;
+	justify-content: center;
+	width: 440px;
+	height: 200px;
+	padding: 0.1rem;
+	background: #ffffff;
+	border-radius: 16px;
 
-  &::after {
-    content: "";
-    display: flex;
-    position: absolute;
-    border-style: solid;
-    border-width: 34px 17px 0;
-    border-color: #ffffff transparent;
-    width: 0;
-    bottom: -34px;
-    left: 46.8%;
-  }
+	&::after {
+		content: "";
+		display: flex;
+		position: absolute;
+		border-style: solid;
+		border-width: 34px 17px 0;
+		border-color: #ffffff transparent;
+		width: 0;
+		bottom: -34px;
+		left: 46.8%;
+	}
 
-  ${media.phone`
+	${media.phone`
     width: 90vw;
   `};
 `;
 
 const ButtonBox = styled.div`
-  display: flex;
-  position: absolute;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  top: 75%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+	display: flex;
+	position: absolute;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	top: 75%;
+	left: 50%;
+	transform: translate(-50%, -50%);
 `;
 
 const TextBox = styled.div`
-  display: flex;
-  position: absolute;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  top: 30%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+	display: flex;
+	position: absolute;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	top: 30%;
+	left: 50%;
+	transform: translate(-50%, -50%);
 `;
 
 export default ChatBoxGroup;
--- frontend/src/components/organisms/Modal.jsx
+++ frontend/src/components/organisms/Modal.jsx
@@ -11,103 +11,93 @@ import Title from "../atoms/Title";
 import TranslucentBackground from "./../atoms/TranslucentBackground";
 import { useEffect } from "react";
 
-const Modal = ({
-  children,
-  modalToggle,
-  setModalToggle,
-  onModalClick,
-  titleText,
-  height,
-  width,
-  maxWidth,
-  flexDirection,
-}) => {
-  useEffect(() => {
-    document.body.style.cssText = `
+const Modal = ({ children, modalToggle, setModalToggle, titleText, height, width, maxWidth, flexDirection }) => {
+	useEffect(() => {
+		document.body.style.cssText = `
       position: absolute; 
       top: -${window.scrollY}px;
 			overflow: hidden;
       width: 100%;`;
-    return () => {
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = "overflow: overlay;";
-      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
-    };
-  }, []);
+		return () => {
+			const scrollY = document.body.style.top;
+			document.body.style.cssText = "overflow: overlay;";
+			window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+		};
+	}, []);
 
-  return (
-    <>
-      {modalToggle && (
-        <>
-          <TranslucentBackground onClick={() => setModalToggle(false)} />
-          <ModalContainer height={height} width={width} maxWidth={maxWidth}>
-            {/* 닫기 버튼 */}
-            <FaWrapper onClick={() => setModalToggle(false)}>
-              <FontAwesomeIcon icon={faX} size="lg" />
-            </FaWrapper>
-            {/* 모달 제목 */}
-            <Title fontSize="2rem" fontWeight="bold" color="black">
-              {titleText}
-            </Title>
-            {/* 모달 Content */}
-            <ContentArea flexDirection={flexDirection}>{children}</ContentArea>
-          </ModalContainer>
-        </>
-      )}
-    </>
-  );
+	return (
+		<>
+			{modalToggle && (
+				<>
+					<TranslucentBackground onClick={() => setModalToggle(false)} />
+					<ModalContainer height={height} width={width} maxWidth={maxWidth}>
+						{/* 닫기 버튼 */}
+						<FaWrapper onClick={() => setModalToggle(false)}>
+							<FontAwesomeIcon icon={faX} size="lg" />
+						</FaWrapper>
+						{/* 모달 제목 */}
+						<Title fontSize="2rem" fontWeight="bold" color="black">
+							{titleText}
+						</Title>
+						{/* 모달 Content */}
+						<ContentArea flexDirection={flexDirection}>{children}</ContentArea>
+					</ModalContainer>
+				</>
+			)}
+		</>
+	);
 };
 
 const ModalContainer = styled.div`
-  display: flex;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  flex-direction: column;
-  z-index: 1001;
-  width: ${(props) => props.width || "55rem"};
-  height: ${(props) => props.height || "80vh"};
-  max-width: ${(props) => props.maxWidth || ""};
-  min-height: 25rem;
-  border-radius: 1rem;
-  background-color: white;
-  align-items: center;
-  box-sizing: border-box;
-  padding: 1.5rem;
+	display: flex;
+	position: fixed;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	flex-direction: column;
+	z-index: 1001;
+	width: ${(props) => props.width || "55rem"};
+	height: ${(props) => props.height || "80vh"};
+	max-width: ${(props) => props.maxWidth || ""};
+	min-height: 25rem;
+	border-radius: 1rem;
+	background-color: white;
+	align-items: center;
+	box-sizing: border-box;
+	padding: 1.5rem;
 
-  ${media.tablet1`
+	${media.tablet1`
     width: 90vw;
   `};
-  ${media.phone`
+	${media.phone`
     width: 90vw;
   `};
 `;
 
 const FaWrapper = styled.div`
-  display: flex;
-  position: absolute;
-  padding: 1rem;
-  right: 1rem;
-  top: 1rem;
-  align-items: center;
-  justify-content: center;
-  pointer: cursor;
-  border-radius: 100%;
-  transition: 0.25s ease-out;
+	display: flex;
+	position: absolute;
+	padding: 1rem;
+	right: 1rem;
+	top: 1rem;
+	align-items: center;
+	justify-content: center;
+	pointer: cursor;
+	border-radius: 100%;
+	transition: 0.25s ease-out;
 
-  &:hover {
-    background-color: rgba(38, 38, 38, 0.5);
-  }
+	&:hover {
+		background-color: rgba(38, 38, 38, 0.25);
+	}
 `;
 
 const ContentArea = styled.div`
-  display: flex;
-  flex-direction: ${(props) => props.flexDirection || "row"};
-  margin: 1.5rem 0 0 0;
-  width: 100%;
-  height: 85%;
-  box-sizing: border-box;
-  overflow: hidden;
+	display: flex;
+	flex-direction: ${(props) => props.flexDirection || "row"};
+	margin: 1.5rem 0 0 0;
+	width: 100%;
+	height: 85%;
+	box-sizing: border-box;
+	overflow: hidden;
 `;
 export default Modal;
