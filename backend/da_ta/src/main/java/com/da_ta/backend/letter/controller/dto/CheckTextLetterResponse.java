1e3b713acc78fb9e7eb0a071571905200e3e5da7
swoody <swoody1101@gmail.com>
2022-11-15 17:17:55 +0900

    feat: CheckTextLetterResponse DTO 추가

---
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/controller/dto/CheckTextLetterResponse.java
@@ -0,0 +1,11 @@
+package com.da_ta.backend.letter.controller.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CheckTextLetterResponse {
+
+    private Boolean isBad;
+}
