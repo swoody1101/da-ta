9d398007459239fb144c353cb311515882a59424
swoody <swoody1101@gmail.com>
2022-11-16 17:20:37 +0900

    feat: 떠다니는 편지 받기 API 수정

--- backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/FloatedLetterRepository.java
+++ backend/da_ta/src/main/java/com/da_ta/backend/letter/domain/repository/FloatedLetterRepository.java
@@ -18,11 +18,6 @@ public interface FloatedLetterRepository extends JpaRepository<FloatedLetter, Lo
             "where f.is_active = true " +
             "and f.recipient_id is null " +
             "and l.writer_id != :recipientId " +
-            "and l.letter_id " +
-            "not in " +
-                "(select la.letter_id " +
-                "from letter_accusation la " +
-                "where la.is_active = true) " +
             "and (l.age_option like concat('%', :ageOption, '%') " +
                 "or l.age_option like concat('%', 'AGE_ALL', '%')) " +
             "and f.floated_letter_id " +
